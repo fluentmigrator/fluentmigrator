708d012c6b6fd53f39ed4cebf33bf875bd29aba8
Nate Kohari <nate@enkari.com>
2008-12-20 04:02:52 +0000

    Fixed problem w/ escaping table names in testing

--- src/FluentMigrator.Tests/Generators/SqlServerGeneratorTests.cs
+++ src/FluentMigrator.Tests/Generators/SqlServerGeneratorTests.cs
@@ -10,7 +10,7 @@ namespace FluentMigrator.Tests.Generators
 	public class SqlServerGeneratorTests
 	{
 		private SqlServerGenerator generator;
-		
+
 		public SqlServerGeneratorTests()
 		{
 			generator = new SqlServerGenerator();
@@ -22,7 +22,7 @@ namespace FluentMigrator.Tests.Generators
 			string tableName = "NewTable";
 			CreateTableExpression expression = GetCreateTableExpression(tableName);
 			string sql = generator.Generate(expression);
-			Assert.Equal("CREATE TABLE NewTable (ColumnName1 NVARCHAR(255), ColumnName2 INT)", sql);
+			Assert.Equal("CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255), ColumnName2 INT)", sql);
 		}
 
 		[Fact]
@@ -32,7 +32,7 @@ namespace FluentMigrator.Tests.Generators
 			CreateTableExpression expression = GetCreateTableExpression(tableName);
 			expression.Columns[0].IsPrimaryKey = true;
 			string sql = generator.Generate(expression);
-			Assert.Equal("CREATE TABLE NewTable (ColumnName1 NVARCHAR(255) PRIMARY KEY CLUSTERED, ColumnName2 INT)", sql);
+			Assert.Equal("CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) PRIMARY KEY CLUSTERED, ColumnName2 INT)", sql);
 		}
 
 		[Fact]
@@ -41,78 +41,78 @@ namespace FluentMigrator.Tests.Generators
 			string tableName = "NewTable";
 			DeleteTableExpression expression = GetDeleteTableExpression(tableName);
 			string sql = generator.Generate(expression);
-			Assert.Equal("DROP TABLE NewTable", sql);
+			Assert.Equal("DROP TABLE [NewTable]", sql);
 		}
 
-        [Fact]
-        public void CanDropColumn()
-        {
-            string tableName = "NewTable";
-            string columnName = "NewColumn";
-            
-            var expression = new DeleteColumnExpression();
-            expression.TableName = tableName;
-            expression.ColumnName = columnName;
+		[Fact]
+		public void CanDropColumn()
+		{
+			string tableName = "NewTable";
+			string columnName = "NewColumn";
 
-            string sql = generator.Generate(expression);
-            Assert.Equal("ALTER TABLE NewTable DROP COLUMN NewColumn", sql);
-        }
+			var expression = new DeleteColumnExpression();
+			expression.TableName = tableName;
+			expression.ColumnName = columnName;
 
-        [Fact]
-        public void CanAddColumn()
-        {
-            string tableName = "NewTable";
-            
-            var columnDefinition = new ColumnDefinition();
-            columnDefinition.Name = "NewColumn";
-            columnDefinition.Size = 5;
-            columnDefinition.Type = DbType.String;
+			string sql = generator.Generate(expression);
+			Assert.Equal("ALTER TABLE [NewTable] DROP COLUMN NewColumn", sql);
+		}
 
-            var expression = new CreateColumnExpression();
-            expression.Column = columnDefinition;
-            expression.TableName = tableName;
+		[Fact]
+		public void CanAddColumn()
+		{
+			string tableName = "NewTable";
 
-            string sql = generator.Generate(expression);
-            Assert.Equal("ALTER TABLE NewTable ADD NewColumn NVARCHAR(5)", sql);
-        }
+			var columnDefinition = new ColumnDefinition();
+			columnDefinition.Name = "NewColumn";
+			columnDefinition.Size = 5;
+			columnDefinition.Type = DbType.String;
 
-        [Fact]
-        public void CanRenameTable()
-        {
-            var expression = new RenameTableExpression();
-            expression.OldName = "Table1";
-            expression.NewName = "Table2";
+			var expression = new CreateColumnExpression();
+			expression.Column = columnDefinition;
+			expression.TableName = tableName;
 
-            string sql = generator.Generate(expression);
-            Assert.Equal("sp_rename [Table1], [Table2]", sql);
-        }
+			string sql = generator.Generate(expression);
+			Assert.Equal("ALTER TABLE [NewTable] ADD NewColumn NVARCHAR(5)", sql);
+		}
 
-        [Fact]
-        public void CanRenameColumn()
-        {
-            var expression = new RenameColumnExpression();
-            expression.TableName = "Table1";
-            expression.OldName = "Column1";
-            expression.NewName = "Column2";
+		[Fact]
+		public void CanRenameTable()
+		{
+			var expression = new RenameTableExpression();
+			expression.OldName = "Table1";
+			expression.NewName = "Table2";
 
-            string sql = generator.Generate(expression);
-            Assert.Equal("sp_rename '[Table1].[Column1]', [Column2]", sql);
-        }
+			string sql = generator.Generate(expression);
+			Assert.Equal("sp_rename [Table1], [Table2]", sql);
+		}
 
-        [Fact]
-        public void CanCreateIndex()
-        {
-            var expression = new CreateIndexExpression();
-            expression.Index.Name = "IX_TEST";
-            expression.Index.TableName = "TEST_TABLE";
-            expression.Index.IsUnique = true;
-            expression.Index.IsClustered = true;
-            expression.Index.Columns.Add(new IndexColumnDefinition { Direction = Direction.Ascending, Name = "Column1" });
-            expression.Index.Columns.Add(new IndexColumnDefinition { Direction = Direction.Descending, Name = "Column2" });
+		[Fact]
+		public void CanRenameColumn()
+		{
+			var expression = new RenameColumnExpression();
+			expression.TableName = "Table1";
+			expression.OldName = "Column1";
+			expression.NewName = "Column2";
 
-            string sql = generator.Generate(expression);
-            Assert.Equal("CREATE UNIQUE CLUSTERED INDEX IX_TEST ON TEST_TABLE (Column1 ASC,Column2 DESC)", sql);
-        }
+			string sql = generator.Generate(expression);
+			Assert.Equal("sp_rename '[Table1].[Column1]', [Column2]", sql);
+		}
+
+		[Fact]
+		public void CanCreateIndex()
+		{
+			var expression = new CreateIndexExpression();
+			expression.Index.Name = "IX_TEST";
+			expression.Index.TableName = "TEST_TABLE";
+			expression.Index.IsUnique = true;
+			expression.Index.IsClustered = true;
+			expression.Index.Columns.Add(new IndexColumnDefinition { Direction = Direction.Ascending, Name = "Column1" });
+			expression.Index.Columns.Add(new IndexColumnDefinition { Direction = Direction.Descending, Name = "Column2" });
+
+			string sql = generator.Generate(expression);
+			Assert.Equal("CREATE UNIQUE CLUSTERED INDEX IX_TEST ON TEST_TABLE (Column1 ASC,Column2 DESC)", sql);
+		}
 
 		private DeleteTableExpression GetDeleteTableExpression(string tableName)
 		{
@@ -124,13 +124,13 @@ namespace FluentMigrator.Tests.Generators
 			string columnName1 = "ColumnName1";
 			string columnName2 = "ColumnName2";
 
-			var column1 = new ColumnDefinition {Name = columnName1, Type = DbType.String};
-			var column2 = new ColumnDefinition {Name = columnName2, Type = DbType.Int32};
+			var column1 = new ColumnDefinition { Name = columnName1, Type = DbType.String };
+			var column2 = new ColumnDefinition { Name = columnName2, Type = DbType.Int32 };
 
 			var expression = new CreateTableExpression { TableName = tableName };
 			expression.Columns.Add(column1);
 			expression.Columns.Add(column2);
 			return expression;
-		}	    
+		}
 	}
 }
