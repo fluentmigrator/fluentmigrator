ac24751bf934f61ae2a9e0f52d4f255d8faa595b
schambers <schambers80@gmail.com>
2009-12-24 09:08:33 -0500

    Added Execute.SqlScript to expression syntax builder

--- src/FluentMigrator/Builders/Execute/IExecuteExpressionRoot.cs
+++ src/FluentMigrator/Builders/Execute/IExecuteExpressionRoot.cs
@@ -1,10 +1,10 @@
-using System;
 using FluentMigrator.Infrastructure;
 
 namespace FluentMigrator.Builders.Execute
 {
-    public interface IExecuteExpressionRoot : IFluentSyntax
-    {
-        void Sql(string sqlStatement);
-    }
+	public interface IExecuteExpressionRoot : IFluentSyntax
+	{
+		void Sql(string sqlStatement);
+		void Script(string sqlScript);
+	}
 }
