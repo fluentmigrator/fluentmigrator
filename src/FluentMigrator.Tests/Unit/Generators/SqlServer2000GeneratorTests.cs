b4eadd8ebc339102c5cba6b13349b94478f16f11
Nathan O'Sullivan <nathan@mammoth.com.au>
2010-10-08 17:51:21 +1100

    Split table creation tests into a subclass for better organisation

--- src/FluentMigrator.Tests/Unit/Generators/SqlServer2000GeneratorTests.cs
+++ src/FluentMigrator.Tests/Unit/Generators/SqlServer2000GeneratorTests.cs
@@ -32,7 +32,7 @@ namespace FluentMigrator.Tests.Unit.Generators
 	[TestFixture]
 	public class SqlServer2000GeneratorTests
 	{
-		private SqlServer2000Generator generator;
+		protected SqlServer2000Generator generator;
 
 		[SetUp]
 		public void SetUp()
@@ -43,20 +43,6 @@ namespace FluentMigrator.Tests.Unit.Generators
 		private DeleteTableExpression GetDeleteTableExpression(string tableName)
 		{
 			return new DeleteTableExpression { TableName = tableName };
-		}
-
-		private CreateTableExpression GetCreateTableExpression(string tableName)
-		{
-			var columnName1 = "ColumnName1";
-			var columnName2 = "ColumnName2";
-
-			var column1 = new ColumnDefinition { Name = columnName1, Type = DbType.String };
-			var column2 = new ColumnDefinition { Name = columnName2, Type = DbType.Int32 };
-
-			var expression = new CreateTableExpression { TableName = tableName };
-			expression.Columns.Add(column1);
-			expression.Columns.Add(column2);
-			return expression;
 		}
 
 		[Test]
@@ -124,39 +110,6 @@ namespace FluentMigrator.Tests.Unit.Generators
 
 			var sql = generator.Generate(expression);
 			sql.ShouldBe("CREATE UNIQUE CLUSTERED INDEX IX_TEST ON [TEST_TABLE] (Column1 ASC,Column2 DESC)");
-		}
-
-		[Test]
-		public void CanCreateTable()
-		{
-			var tableName = "NewTable";
-			var expression = GetCreateTableExpression(tableName);
-			var sql = generator.Generate(expression);
-			sql.ShouldBe("CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL, ColumnName2 INT NOT NULL)");
-		}
-
-		[Test]
-		public void CanCreateTableWithCustomColumnType()
-		{
-			var tableName = "NewTable";
-			var expression = GetCreateTableExpression(tableName);
-			expression.Columns[0].IsPrimaryKey = true;
-			expression.Columns[1].Type = null;
-			expression.Columns[1].CustomType = "[timestamp]";
-			var sql = generator.Generate(expression);
-			sql.ShouldBe(
-				"CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL PRIMARY KEY CLUSTERED, ColumnName2 [timestamp] NOT NULL)");
-		}
-
-		[Test]
-		public void CanCreateTableWithPrimaryKey()
-		{
-			var tableName = "NewTable";
-			var expression = GetCreateTableExpression(tableName);
-			expression.Columns[0].IsPrimaryKey = true;
-			var sql = generator.Generate(expression);
-			sql.ShouldBe(
-				"CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL PRIMARY KEY CLUSTERED, ColumnName2 INT NOT NULL)");
 		}
 
 		[Test]
@@ -293,6 +246,57 @@ namespace FluentMigrator.Tests.Unit.Generators
 
 			var sql = generator.Generate(expression);
 			sql.ShouldNotBeNull();
+		}
+	}
+
+	[TestFixture]
+	public class SqlServer200GeneratorCreateTableTests : SqlServer2000GeneratorTests
+	{
+		[Test]
+		public void CanCreateTable()
+		{
+			var tableName = "NewTable";
+			var expression = GetCreateTableExpression(tableName);
+			var sql = generator.Generate(expression);
+			sql.ShouldBe("CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL, ColumnName2 INT NOT NULL)");
+		}
+
+		[Test]
+		public void CanCreateTableWithCustomColumnType()
+		{
+			var tableName = "NewTable";
+			var expression = GetCreateTableExpression(tableName);
+			expression.Columns[0].IsPrimaryKey = true;
+			expression.Columns[1].Type = null;
+			expression.Columns[1].CustomType = "[timestamp]";
+			var sql = generator.Generate(expression);
+			sql.ShouldBe(
+				"CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL PRIMARY KEY CLUSTERED, ColumnName2 [timestamp] NOT NULL)");
+		}
+
+		[Test]
+		public void CanCreateTableWithPrimaryKey()
+		{
+			var tableName = "NewTable";
+			var expression = GetCreateTableExpression(tableName);
+			expression.Columns[0].IsPrimaryKey = true;
+			var sql = generator.Generate(expression);
+			sql.ShouldBe(
+				"CREATE TABLE [NewTable] (ColumnName1 NVARCHAR(255) NOT NULL PRIMARY KEY CLUSTERED, ColumnName2 INT NOT NULL)");
+		}
+
+		protected CreateTableExpression GetCreateTableExpression(string tableName)
+		{
+			var columnName1 = "ColumnName1";
+			var columnName2 = "ColumnName2";
+
+			var column1 = new ColumnDefinition { Name = columnName1, Type = DbType.String };
+			var column2 = new ColumnDefinition { Name = columnName2, Type = DbType.Int32 };
+
+			var expression = new CreateTableExpression { TableName = tableName };
+			expression.Columns.Add(column1);
+			expression.Columns.Add(column2);
+			return expression;
 		}
 	}
 }
