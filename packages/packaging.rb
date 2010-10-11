8f4edc68325943d2be416d31f9712a14c356e60b
Troy Goode <troygoode@gmail.com>
2010-10-11 09:52:59 -0400

    Removed the ILMerge portion of the rake script. NuPack package now contains all dependencies.

--- packages/packaging.rb
+++ packages/packaging.rb
@@ -1,51 +1,19 @@
 require 'fileutils'
 
-exec :ilmerge_runner_with_providers => :release do |cmd|
-  FileUtils.mkdir_p 'il_merged'
-  cmd.path_to_command = "tools/ILMerge.exe"
-  cmd.parameters [
-  	'/target:library',
-  	'/out:il_merged\\FluentMigrator.Runner.dll',
-  	'/zeroPeKind', # necessary for SQLLite, which isn't managed. might not work though...
-  	'src\\FluentMigrator\\bin\\Release\\FluentMigrator.dll',
-  	'src\\FluentMigrator.Runner\\bin\\Release\\FluentMigrator.Runner.dll',
-  	'lib\\MySql.Data.dll',
-  	'lib\\System.Data.SQLite.DLL'
-  ]
-end
-
-exec :ilmerge_console_with_runner => :ilmerge_runner_with_providers do |cmd|
-  cmd.path_to_command = "tools/ILMerge.exe"
-  cmd.parameters [
-  	'/target:winexe',
-  	'/out:il_merged\\Migrate.exe',
-  	'/zeroPeKind', # necessary for SQLLite, which isn't managed. might not work though...
-  	'src\\FluentMigrator.Console\\bin\\x86\\Release\\Migrate.exe',
-  	'il_merged\\FluentMigrator.Runner.dll'
-  ]
-end
-
-exec :ilmerge_nant_with_runner => :ilmerge_runner_with_providers do |cmd|
-  cmd.path_to_command = "tools/ILMerge.exe"
-  cmd.parameters [
-  	'/target:library',
-  	'/out:il_merged\\FluentMigrator.NAnt.dll',
-  	'/zeroPeKind', # necessary for SQLLite, which isn't managed. might not work though...
-  	'src\\FluentMigrator.NAnt\\bin\\Release\\FluentMigrator.NAnt.dll',
-  	'il_merged\\FluentMigrator.Runner.dll'
-  ]
-end
-
-exec :ilmerge_msbuild_with_runner => :ilmerge_runner_with_providers do |cmd|
-  cmd.path_to_command = "tools/ILMerge.exe"
-  cmd.parameters [
-  	'/target:library',
-  	'/out:il_merged\\FluentMigrator.MSBuild.dll',
-  	'/zeroPeKind', # necessary for SQLLite, which isn't managed. might not work though...
-  	'src\\FluentMigrator.MSBuild\\bin\\Release\\FluentMigrator.MSBuild.dll',
-  	'il_merged\\FluentMigrator.Runner.dll'
-  ]
-end
+#Unfortunately SQLite cannot be ILMerged in because it is not 100% managed.
+#exec :ilmerge_runner_with_providers => :release do |cmd|
+#  FileUtils.mkdir_p 'il_merged'
+#  cmd.path_to_command = "tools/ILMerge.exe"
+#  cmd.parameters [
+#  	'/target:library',
+#  	'/out:il_merged\\FluentMigrator.Runner.dll',
+#  	'/zeroPeKind', # necessary for SQLLite, which isn't managed. might not work though...
+#  	'src\\FluentMigrator\\bin\\Release\\FluentMigrator.dll',
+#  	'src\\FluentMigrator.Runner\\bin\\Release\\FluentMigrator.Runner.dll',
+#  	'lib\\MySql.Data.dll',
+#  	'lib\\System.Data.SQLite.DLL'
+#  ]
+#end
 
 def copy_files(from, to, filename, extensions)
 	extensions.each do |ext|
@@ -53,19 +21,21 @@ def copy_files(from, to, filename, extensions)
 	end
 end
 
-task :prepare_package => [:ilmerge_console_with_runner,:ilmerge_nant_with_runner,:ilmerge_msbuild_with_runner] do
+task :prepare_package => :release do
   output_directory_lib = './packages/FluentMigrator/lib/35/'
   output_directory_tools = './packages/FluentMigrator/tools/'
   FileUtils.mkdir_p output_directory_lib
   FileUtils.mkdir_p output_directory_tools
 
   copy_files './src/FluentMigrator/bin/Release/', output_directory_lib, 'FluentMigrator', ['dll', 'pdb', 'xml']
-  copy_files './il_merged/', output_directory_tools, 'Migrate', ['exe']
-  copy_files './src/FluentMigrator.Console/bin/x86/Release/', output_directory_tools, 'Migrate', ['exe.config']
-  copy_files './il_merged/', output_directory_tools, 'FluentMigrator.NAnt', ['dll']
-  copy_files './il_merged/', output_directory_tools, 'FluentMigrator.MSBuild', ['dll']
 
-  FileUtils.remove_dir './il_merged/'
+  copy_files './lib/', output_directory_tools, 'MySql.Data', ['dll']
+  copy_files './lib/', output_directory_tools, 'System.Data.SQLite', ['dll']
+  copy_files './src/FluentMigrator.Runner/bin/Release/', output_directory_tools, 'FluentMigrator.Runner', ['dll']
+  copy_files './src/FluentMigrator.Console/bin/x86/Release/', output_directory_tools, 'Migrate', ['exe']
+  copy_files './src/FluentMigrator.Console/bin/x86/Release/', output_directory_tools, 'Migrate', ['exe.config']
+  copy_files './src/FluentMigrator.Nant/bin/Release/', output_directory_tools, 'FluentMigrator.NAnt', ['dll']
+  copy_files './src/FluentMigrator.MSBuild/bin/Release/', output_directory_tools, 'FluentMigrator.MSBuild', ['dll']
 end
 
 exec :package => :prepare_package do |cmd|
