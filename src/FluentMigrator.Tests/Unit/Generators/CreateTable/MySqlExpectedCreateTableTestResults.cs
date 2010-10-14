61ff2c755c37090a34e260f351b069c6ed8b1c57
Nathan O'Sullivan <nathan@mammoth.com.au>
2010-10-15 09:01:00 +1100

    Convert MysqlGeneratorTests to implemented ExpectedResults classes, use the new testbase hierarchy

---
+++ src/FluentMigrator.Tests/Unit/Generators/CreateTable/MySqlExpectedCreateTableTestResults.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner.Generators;
+
+namespace FluentMigrator.Tests.Unit.Generators.CreateTable
+{
+	public class MySqlCreateTableTests : GeneratorCreateTableTestsBase<MySqlGenerator,MySqlExpectedCreateTableTestResults>
+	{
+	}
+
+	public class MySqlExpectedCreateTableTestResults : IExpectedCreateTableTestResults
+	{
+		public string CreateTable()
+		{
+			return "CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) NOT NULL) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithCustomColumnType()
+		{
+			return
+				"CREATE TABLE `NewTable` (ColumnName1 [timestamp] NOT NULL) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithPrimaryKey()
+		{
+			return
+				"CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) NOT NULL , PRIMARY KEY (`ColumnName1`)) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithIdentity()
+		{
+			return
+				"CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) NOT NULL AUTO_INCREMENT) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithNullField()
+		{
+			return "CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255)) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithDefaultValue()
+		{
+			return
+				"CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) NOT NULL DEFAULT 'Default', ColumnName2 INTEGER NOT NULL DEFAULT 0) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithDefaultValueExplicitlySetToNull()
+		{
+			return "CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) DEFAULT NULL) ENGINE = INNODB";
+		}
+
+		public string CreateTableWithMultipleColumns()
+		{
+			return "CREATE TABLE `NewTable` (ColumnName1 VARCHAR(255) NOT NULL, ColumnName2 INTEGER NOT NULL, ColumnName3 VARCHAR(255) NOT NULL) ENGINE = INNODB";
+		}
+	}
+}
---
+++ src/FluentMigrator.Tests/Unit/Generators/MySqlExpectedTestResults.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using FluentMigrator.Runner.Generators;
+using NUnit.Framework;
+
+namespace FluentMigrator.Tests.Unit.Generators
+{
+	public class MySqlGeneratorTests : GeneratorTestsBase<MySqlGenerator, MySqlExpectedTestResults>
+	{
+		[ExpectedException(typeof(NotImplementedException))]
+		public override void CanRenameColumn()
+		{
+			base.CanRenameColumn();
+		}
+	}
+
+	public class MySqlExpectedTestResults : IExpectedTestResults
+	{
+		public string AddColumn()
+		{
+			return "ALTER TABLE `NewTable` ADD NewColumn VARCHAR(5) NOT NULL";
+		}
+
+		public string AddDecimalColumn()
+		{
+			return "ALTER TABLE `NewTable` ADD NewColumn DECIMAL(19,2) NOT NULL";
+		}
+
+		public string CreateForeignKey()
+		{
+			return
+				"ALTER TABLE `TestForeignTable` ADD CONSTRAINT FK_Test FOREIGN KEY (Column3,Column4) REFERENCES TestPrimaryTable (Column1,Column2)";
+		}
+
+		public string CreateUniqueClusteredIndex()
+		{
+			return "CREATE UNIQUE INDEX IX_TEST ON TEST_TABLE (Column1 ASC,Column2 DESC)";
+		}
+
+		public string DropForeignKey()
+		{
+			return "ALTER TABLE `TestPrimaryTable` DROP FOREIGN KEY `FK_Test`";
+		}
+
+		public string DropTable()
+		{
+			return "DROP TABLE `NewTable`";
+		}
+
+		public string InsertData()
+		{
+			return @"INSERT INTO `TestTable` (Id,Name,Website) VALUES (1,'Just''in','codethinked.com');" +
+					@"INSERT INTO `TestTable` (Id,Name,Website) VALUES (2,'Na\\te','kohari.org');";
+		}
+
+		public string InsertGuidData()
+		{
+			return "INSERT INTO `TestTable` (guid) VALUES ('12345678-1234-1234-1234-123456789012');";
+		}
+
+		public string RenameColumn()
+		{
+			return "not implemented";
+		}
+
+		public string RenameTable()
+		{
+			return "RENAME TABLE `Table1` TO `Table2`";
+		}
+
+		public string DropColumn()
+		{
+			return "ALTER TABLE `NewTable` DROP COLUMN NewColumn";
+		}
+	}
+}
