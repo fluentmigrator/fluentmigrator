90c295dcef641f0c6b9a4b093ed2e7334a83e793
Nathan O'Sullivan <nathan@mammoth.com.au>
2010-10-19 06:48:49 +1100

    Convert OracleGeneratorTests to implemented ExpectedResults classes, use the new testbase hierarchy

---
+++ src/FluentMigrator.Tests/Unit/Generators/CreateTable/OracleExpectedCreateTableResults.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using FluentMigrator.Runner.Generators;
+using NUnit.Framework;
+using NUnit.Should;
+
+namespace FluentMigrator.Tests.Unit.Generators.CreateTable
+{
+	public class OracleCreateTableGeneratorTests : GeneratorCreateTableTestsBase<OracleGenerator,OracleExpectedCreateTableResults>
+	{
+		[ExpectedException(typeof(NotImplementedException))]
+		public override void CanCreateTableWithIdentity()
+		{
+			base.CanCreateTableWithIdentity();
+		}
+	}
+
+	public class OracleExpectedCreateTableResults : IExpectedCreateTableTestResults
+	{
+		public string CreateTable()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) NOT NULL)";
+		}
+
+		public string CreateTableWithCustomColumnType()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 timestamp NOT NULL)";
+		}
+
+		public string CreateTableWithPrimaryKey()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) NOT NULL PRIMARY KEY)";
+		}
+
+		public string CreateTableWithIdentity()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string CreateTableWithNullField()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255))";
+		}
+
+		public string CreateTableWithDefaultValue()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) DEFAULT 'Default' NOT NULL, ColumnName2 NUMBER(10,0) DEFAULT 0 NOT NULL)";
+		}
+
+		public string CreateTableWithDefaultValueExplicitlySetToNull()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) DEFAULT NULL)";
+		}
+
+		public string CreateTableWithMultipleColumns()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) NOT NULL, ColumnName2 NUMBER(10,0) NOT NULL, ColumnName3 NVARCHAR2(255) NOT NULL)";
+		}
+
+		public string CreateTableWithMultipartPrimaryKey()
+		{
+			// See the note in OracleColumn about why the PK should not be named
+			return
+				"CREATE TABLE NewTable (ColumnName1 NVARCHAR2(255) NOT NULL, ColumnName2 NUMBER(10,0) NOT NULL,  PRIMARY KEY (ColumnName1,ColumnName2))";
+		}
+	}
+}
---
+++ src/FluentMigrator.Tests/Unit/Generators/OracleExpectedTestResults.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner.Generators;
+
+namespace FluentMigrator.Tests.Unit.Generators
+{
+	public class OracleGeneratorTests : GeneratorTestsBase<OracleGenerator,OracleExpectedTestResults>
+	{
+	}
+
+	public class OracleExpectedTestResults : IExpectedTestResults
+	{
+		public string AddColumn()
+		{
+			return "ALTER TABLE NewTable ADD NewColumn NVARCHAR2(5) NOT NULL";
+		}
+
+		public string AddDecimalColumn()
+		{
+			return "ALTER TABLE NewTable ADD NewColumn NUMBER(19,2) NOT NULL";
+		}
+
+		public string CreateForeignKey()
+		{
+			return "ALTER TABLE TestForeignTable ADD CONSTRAINT FK_Test FOREIGN KEY (Column3,Column4) REFERENCES TestPrimaryTable (Column1,Column2)";
+		}
+
+		public string CreateUniqueClusteredIndex()
+		{
+			return "CREATE UNIQUE INDEX IX_TEST ON TEST_TABLE (Column1 ASC,Column2 DESC)";
+		}
+
+		public string DropForeignKey()
+		{
+			return "ALTER TABLE TestPrimaryTable DROP CONSTRAINT FK_Test";
+		}
+
+		public string DropTable()
+		{
+			return "DROP TABLE NewTable";
+		}
+
+		public string InsertData()
+		{
+			return @"INSERT ALL INTO TestTable (Id,Name,Website) VALUES (1,'Just''in','codethinked.com')" +
+			       @" INTO TestTable (Id,Name,Website) VALUES (2,'Na\te','kohari.org')" +
+			       @" SELECT 1 FROM DUAL";
+		}
+
+		public string InsertGuidData()
+		{
+			return "INSERT ALL INTO TestTable (guid) VALUES ('12345678-1234-1234-1234-123456789012') SELECT 1 FROM DUAL";
+		}
+
+		public string RenameColumn()
+		{
+			return "ALTER TABLE Table1 RENAME COLUMN Column1 TO Column2";
+		}
+
+		public string RenameTable()
+		{
+			return "ALTER TABLE Table1 RENAME TO Table2";
+		}
+
+		public string DropColumn()
+		{
+			return "ALTER TABLE NewTable DROP COLUMN NewColumn";
+		}
+	}
+}
