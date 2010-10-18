6604120ceb2d659a992d65818444f65861ec1bf4
Nathan O'Sullivan <nathan@mammoth.com.au>
2010-10-19 07:42:25 +1100

    Convert SqliteGeneratorTests to implemented ExpectedResults classes, use the new testbase hierarchy

---
+++ src/FluentMigrator.Tests/Unit/Generators/CreateTable/SqliteExpectedCreateTableTestResults.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner.Generators;
+using NUnit.Framework;
+
+namespace FluentMigrator.Tests.Unit.Generators.CreateTable
+{
+	public class SqliteGeneratorCreateTableTests : GeneratorCreateTableTestsBase<SqliteGenerator,SqliteExpectedCreateTableTestResults>
+	{
+		[Ignore("Sqlite requires primary key to be set on identity column")]
+		public override void CanCreateTableWithIdentity()
+		{
+			base.CanCreateTableWithIdentity();
+		}
+	}
+
+	public class SqliteExpectedCreateTableTestResults : IExpectedCreateTableTestResults
+	{
+		public string CreateTable()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT NOT NULL)";
+		}
+
+		public string CreateTableWithCustomColumnType()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 timestamp NOT NULL)";
+		}
+
+		public string CreateTableWithPrimaryKey()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT NOT NULL PRIMARY KEY)";
+		}
+
+		public string CreateTableWithIdentity()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)";
+		}
+
+		public string CreateTableWithNullField()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT)";
+		}
+
+		public string CreateTableWithDefaultValue()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT NOT NULL DEFAULT 'Default', ColumnName2 INTEGER NOT NULL DEFAULT 0)";
+		}
+
+		public string CreateTableWithDefaultValueExplicitlySetToNull()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT DEFAULT NULL)";
+		}
+
+		public string CreateTableWithMultipleColumns()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT NOT NULL, ColumnName2 INTEGER NOT NULL, ColumnName3 TEXT NOT NULL)";
+		}
+
+		public string CreateTableWithMultipartPrimaryKey()
+		{
+			return "CREATE TABLE NewTable (ColumnName1 TEXT NOT NULL, ColumnName2 INTEGER NOT NULL, CONSTRAINT ColumnName1_ColumnName2_PK PRIMARY KEY (ColumnName1,ColumnName2))";
+		}
+	}
+}
---
+++ src/FluentMigrator.Tests/Unit/Generators/SqliteExpectedTestResults.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner.Generators;
+using NUnit.Framework;
+
+namespace FluentMigrator.Tests.Unit.Generators
+{
+	public class SqliteGeneratorTests : GeneratorTestsBase<SqliteGenerator,SqliteExpectedTestResults>
+	{
+		[ExpectedException(typeof(NotImplementedException))]
+		public override void CanRenameColumn()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class SqliteExpectedTestResults : IExpectedTestResults
+	{
+		public string AddColumn()
+		{
+			return "ALTER TABLE [NewTable] ADD COLUMN NewColumn TEXT NOT NULL";
+		}
+
+		public string AddDecimalColumn()
+		{
+			return "ALTER TABLE [NewTable] ADD COLUMN NewColumn NUMERIC NOT NULL";
+		}
+
+		public string CreateForeignKey()
+		{
+			// The current implementation chooses to do nothing with FK's instead of throwing
+			return string.Empty;
+		}
+
+		public string CreateUniqueClusteredIndex()
+		{
+			return "CREATE UNIQUE INDEX IF NOT EXISTS IX_TEST ON TEST_TABLE (Column1,Column2)";
+		}
+
+		public string DropForeignKey()
+		{
+			// The current implementation chooses to do nothing with FK's instead of throwing
+			return string.Empty;
+		}
+
+		public string DropTable()
+		{
+			return "DROP TABLE NewTable";
+		}
+
+		public string InsertData()
+		{
+			return @"INSERT INTO [TestTable] (Id,Name,Website) VALUES (1,'Just''in','codethinked.com');" +
+				@"INSERT INTO [TestTable] (Id,Name,Website) VALUES (2,'Na\te','kohari.org');";
+		}
+
+		public string InsertGuidData()
+		{
+			return @"INSERT INTO [TestTable] (guid) VALUES ('12345678-1234-1234-1234-123456789012');";
+		}
+
+		public string RenameColumn()
+		{
+			return string.Empty;
+		}
+
+		public string RenameTable()
+		{
+			return "ALTER TABLE Table1 RENAME TO Table2";
+		}
+
+		public string DropColumn()
+		{
+			return "ALTER TABLE NewTable DROP COLUMN NewColumn";
+		}
+	}
+}
