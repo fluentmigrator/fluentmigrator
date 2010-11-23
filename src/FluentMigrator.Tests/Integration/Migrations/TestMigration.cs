5ae629840d19f7b91ff3962bd9acd5e0b1478e53
jtreuting <jeff@truburn.net>
2010-11-23 09:29:08 -0800

    Alter table schema in TestMigration was throwing exception

--- src/FluentMigrator.Tests/Integration/Migrations/TestMigration.cs
+++ src/FluentMigrator.Tests/Integration/Migrations/TestMigration.cs
@@ -48,7 +48,8 @@ namespace FluentMigrator.Tests.Integration.Migrations
 
 			Rename.Table("Foo").To("Bar");
 
-      Alter.Table( "" ).InSchema( "" ).ToSchema( "" );
+            // commented out because this throws not implemented exception
+            //Alter.Table( "" ).InSchema( "" ).ToSchema( "" );
 
 			//does not work in sqlite
 			//Rename.Column("Fizz").OnTable("Bar").To("Buzz");
