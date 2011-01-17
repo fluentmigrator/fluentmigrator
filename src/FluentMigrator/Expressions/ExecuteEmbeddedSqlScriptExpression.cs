e8127778a6ac61c788014d56a0d49abfeb5b9869
abusby <andrew.busby@aimstrategic.com>
2011-01-17 14:12:24 +0000

    Added Embedded Sql Script Runner to allow Sql script to be embedded in the migration assembly

---
+++ src/FluentMigrator/Expressions/ExecuteEmbeddedSqlScriptExpression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Infrastructure;
+using System.Reflection;
+using System.IO;
+
+namespace FluentMigrator.Expressions
+{
+    public class ExecuteEmbeddedSqlScriptExpression : MigrationExpressionBase
+    {
+        public string SqlScript { get; set; }
+
+        public override void ExecuteWith(IMigrationProcessor processor)
+        {
+            string sqlText;
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(SqlScript))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    sqlText = reader.ReadToEnd();
+                }
+            }
+            processor.Execute(sqlText);
+        }
+
+        public override void ApplyConventions(IMigrationConventions conventions)
+        {
+            var resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            var fullManifestPath = resources.SkipWhile(x => !x.EndsWith(SqlScript));           
+            SqlScript = fullManifestPath.FirstOrDefault();
+        }
+
+        public override void CollectValidationErrors(ICollection<string> errors)
+        {
+            if (string.IsNullOrEmpty(SqlScript))
+                errors.Add(ErrorMessages.SqlScriptCannotBeNullOrEmpty);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + SqlScript;
+        }
+    }
+}
