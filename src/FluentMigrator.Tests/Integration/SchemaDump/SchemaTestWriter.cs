7eac247c20b8892b316cbbaaddbfe4c4408a3571
josh <joshcoffman@gmail.com>
2011-02-01 15:32:55 -0700

    added WriteToStream code

--- src/FluentMigrator.Tests/Integration/SchemaDump/SchemaTestWriter.cs
+++ src/FluentMigrator.Tests/Integration/SchemaDump/SchemaTestWriter.cs
@@ -8,7 +8,16 @@ using FluentMigrator.Model;
 namespace FluentMigrator.Tests.Integration.SchemaDump {
     public class SchemaTestWriter : FluentMigrator.Runner.SchemaWriters.SchemaWriterBase {
         public override void WriteToStream(ICollection<TableDefinition> tables, System.IO.StreamWriter output) {
-            throw new NotImplementedException();
+            int tableCount = 0, indexCount = 0, keyCount = 0, columnCount = 0;
+            foreach (TableDefinition table in tables) {
+                tableCount++;
+                foreach (ColumnDefinition column in table.Columns) { columnCount++; }
+                foreach (IndexDefinition index in table.Indexes) { indexCount++; }
+                foreach (ForeignKeyDefinition fkey in table.ForiengKeys) { keyCount++; }
+            }
+
+            output.WriteLine(String.Format("tables: {0}; columns: {1}; indexes: {2}; keys: {3}", 
+                tableCount, columnCount, indexCount, keyCount));
         }
     }
 }
