bdf5ffcf7111fbff581d53726bf7a5c168c59864
josh <joshcoffman@gmail.com>
2011-02-01 14:28:49 -0700

    adding initial, nonfunctional schema writer

---
+++ src/FluentMigrator.Runner/SchemaWriters/RawSchemaWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using FluentMigrator;
+
+namespace FluentMigrator.Runner.SchemaWriters {
+    public class SchemaWriterBase : ISchemaWriter {
+        public void WriteToStream(ICollection<Model.TableDefinition> tables, System.IO.StreamWriter output) {
+            throw new NotImplementedException();
+        }
+
+        public abstract void WriteToFile(ICollection<Model.TableDefinition> tables, string file) {
+            throw new NotImplementedException();
+        }
+    }
+}
