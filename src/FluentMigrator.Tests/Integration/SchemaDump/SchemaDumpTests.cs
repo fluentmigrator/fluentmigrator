68c27e644bc2705b2678a239ac4de312aad607b2
josh <joshcoffman@gmail.com>
2011-02-01 15:38:19 -0700

    added SchemaDumpTests class

---
+++ src/FluentMigrator.Tests/Integration/SchemaDump/SchemaDumpTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Generators;
+using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.MySql;
+using FluentMigrator.Runner.Processors.Sqlite;
+using FluentMigrator.Runner.Processors.SqlServer;
+using MySql.Data.MySqlClient;
+using FluentMigrator.Tests.Helpers;
+using NUnit.Framework;
+using NUnit.Should;
+
+namespace FluentMigrator.Tests.Integration.SchemaDump {
+
+    [TestFixture]
+    public class SchemaDumpTests {
+        [SetUp]
+        public void SetUp() {
+        }
+
+        [TearDown]
+        public void TearDown() {
+        }
+    }
+}
