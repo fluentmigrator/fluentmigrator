674fd1b06c8b6032606693a3f8cb55970d630d47
josh <joshcoffman@gmail.com>
2011-02-01 14:14:03 -0700

    added schema writer interface

---
+++ src/FluentMigrator/ISchemaWriter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentMigrator {
+    public class ISchemaWriter {
+        void WriteToFile(ICollection<Model.TableDefinition> tables, string file);
+    }
+}
