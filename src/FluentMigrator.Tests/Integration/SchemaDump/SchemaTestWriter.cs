c95985f4128e3097f004a7092a5e8c3c8dbfeae0
josh <joshcoffman@gmail.com>
2011-02-01 15:21:55 -0700

    inherit SchemaWriterBase, implement WriteToStream

--- src/FluentMigrator.Tests/Integration/SchemaDump/SchemaTestWriter.cs
+++ src/FluentMigrator.Tests/Integration/SchemaDump/SchemaTestWriter.cs
@@ -6,6 +6,9 @@ using FluentMigrator;
 using FluentMigrator.Model;
 
 namespace FluentMigrator.Tests.Integration.SchemaDump {
-    public class SchemaTestWriter {
+    public class SchemaTestWriter : FluentMigrator.Runner.SchemaWriters.SchemaWriterBase {
+        public override void WriteToStream(ICollection<TableDefinition> tables, System.IO.StreamWriter output) {
+            throw new NotImplementedException();
+        }
     }
 }
