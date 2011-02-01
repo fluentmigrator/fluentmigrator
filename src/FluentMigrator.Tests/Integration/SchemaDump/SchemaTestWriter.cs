f8e68bb36ff1e39f459c362aaa054ba73b0e67a4
josh <joshcoffman@gmail.com>
2011-02-01 15:18:05 -0700

    schema test writer

---
+++ src/FluentMigrator.Tests/Integration/SchemaDump/SchemaTestWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+using FluentMigrator.Model;
+
+namespace FluentMigrator.Tests.Integration.SchemaDump {
+    public class SchemaTestWriter {
+    }
+}
