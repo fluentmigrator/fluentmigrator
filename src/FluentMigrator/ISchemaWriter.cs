bd18a9754d1c4c22746589443ab0c66afe84a1ce
josh <joshcoffman@gmail.com>
2011-02-01 14:23:15 -0700

    added WriteToStream on SchemaWriter

--- src/FluentMigrator/ISchemaWriter.cs
+++ src/FluentMigrator/ISchemaWriter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace FluentMigrator {
-    public class ISchemaWriter {
+    public interface ISchemaWriter {
+        void WriteToStream(ICollection<Model.TableDefinition> tables, StreamWriter output);
         void WriteToFile(ICollection<Model.TableDefinition> tables, string file);
     }
 }
