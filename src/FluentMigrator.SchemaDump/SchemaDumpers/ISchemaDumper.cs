32630fb31773c405d1962f98805756bd248cc303
josh <joshcoffman@gmail.com>
2011-02-21 14:16:52 -0700

    ha! forgot to re-add moved code

---
+++ src/FluentMigrator.SchemaDump/SchemaDumpers/ISchemaDumper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Model;
+
+namespace FluentMigrator.SchemaDump.SchemaDumpers {
+    interface ISchemaDumper {
+        IList<TableDefinition> ReadDbSchema();
+    }
+}
