bac9819be5685fd3e3d2f232b03c207f10fcc49d
abusby <andrew.busby@aimstrategic.com>
2011-03-03 19:25:08 +0000

    Changed the default timeout time to 30 seconds. Currently it is unlimited which is obviously dangerous2

--- src/FluentMigrator.Runner/Processors/ProcessorOptions.cs
+++ src/FluentMigrator.Runner/Processors/ProcessorOptions.cs
@@ -2,6 +2,14 @@
 {
 	public class ProcessorOptions : IMigrationProcessorOptions
 	{
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ProcessorOptions"/> class.
+        /// </summary>
+        public ProcessorOptions()
+        {
+            Timeout = 30;    
+        }
+
 		public bool PreviewOnly { get; set; }
 		public int Timeout { get; set; }
 	}
