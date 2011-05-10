829b2a819f33babf36f9457658045f96e013c454
Thomas G Mayfield <tgmayfield@gmail.com>
2011-05-10 13:21:48 -0600

    Identify problem with nested namespaces
    - I organize my migrations with my mappings, so that my FluentNHibernate mapping and my migration each share a namespace inside the main migrations namespace or project

---
+++ src/FluentMigrator.Tests/Integration/Migrations/Nested/Group1/FromGroup1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FluentMigrator.Tests.Integration.Migrations.Nested.Group1
+{
+	[Migration(101)]
+	public class FromGroup1
+		: Migration
+	{
+		public override void Up()
+		{
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
