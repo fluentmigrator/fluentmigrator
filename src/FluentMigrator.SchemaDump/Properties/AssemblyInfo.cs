9640e00bbf60bd5166ada136cfd2231f5def6978
abusby <andrew.busby@aimstrategic.com>
2011-07-10 03:26:45 +0100

    Added Placeholder attributes to AssemblyInfo.cs Files.  This will probably break the NANT build task

--- src/FluentMigrator.SchemaDump/Properties/AssemblyInfo.cs
+++ src/FluentMigrator.SchemaDump/Properties/AssemblyInfo.cs
@@ -1,27 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FluentMigrator.SchemaDump")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FluentMigrator.SchemaDump")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("be5bd301-3c78-4c9d-8168-0bb3a4fee382")]
-
+[assembly: AssemblyTitle("FluentMigrator")]
+[assembly: AssemblyDescription("FluentMigrator")]
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,8 +12,16 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.11016.0")]
+[assembly: AssemblyFileVersion("1.0.11016.0")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aim Strategic Management Limited - Strategy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aim Strategic Management Limited")]
+[assembly: AssemblyProduct("Aim Strategic Management Limited - Strategy")]
+[assembly: AssemblyCopyright("© Aim Strategic Management Limited - All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
--- src/FluentMigrator.Tests/Properties/AssemblyInfo.cs
+++ src/FluentMigrator.Tests/Properties/AssemblyInfo.cs
@@ -1,52 +1,27 @@
-#region License
-// 
-// Copyright (c) 2007-2009, Sean Chambers <schambers80@gmail.com>
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
 
 using System.Reflection;
-using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentMigrator")]
+[assembly: AssemblyDescription("FluentMigrator")]
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.11016.0")]
+[assembly: AssemblyFileVersion("1.0.11016.0")]
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluentMigrator.Tests")]
+[assembly: AssemblyTitle("Aim Strategic Management Limited - Strategy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FluentMigrator.Tests")]
-[assembly: AssemblyCopyright("Copyright � Microsoft 2008")]
+[assembly: AssemblyCompany("Aim Strategic Management Limited")]
+[assembly: AssemblyProduct("Aim Strategic Management Limited - Strategy")]
+[assembly: AssemblyCopyright("© Aim Strategic Management Limited - All rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62c03e52-5509-4648-b1ca-2197b11e9f78")]
-
-// Version information for an assembly consists of the following four values:
-//
-//	  Major Version
-//	  Minor Version 
-//	  Build Number
-//	  Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
