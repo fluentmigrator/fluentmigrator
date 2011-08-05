1724bc860a0b79ddb7f81064edfe272779404d84
tkellogg <tkellogg@kpaonline.com>
2011-08-04 22:41:38 -0600

    filled in some tests, added fixtures

--- src/FluentMigrator.Tests/Unit/ConnectionStringName/ConnectionStringNameTests.cs
+++ src/FluentMigrator.Tests/Unit/ConnectionStringName/ConnectionStringNameTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using FluentMigrator.Runner.Initialization;
+using System.IO;
 
 namespace FluentMigrator.Tests.Unit.ConnectionStringName
 {
@@ -20,14 +21,25 @@ namespace FluentMigrator.Tests.Unit.ConnectionStringName
 		[Test]
 		public void ItFirstTriesConfigPath()
 		{
-			var sut = new NetConfigManager(GetPath(""), null);
+			var sut = new NetConfigManager(GetPath("WithConnectionString.config"), null);
 			var result = sut.GetConnectionString(CONNECTION_NAME);
+			Assert.That(result, Is.EqualTo("From Arbitrary Config"));
 		}
 
 		[Test]
-		public void ItFailsIfTheConfigPathWasSpecifiedButCouldntResolve()
+		public void ItFailsIfTheConfigPathWasSpecifiedButCouldntResolveString()
 		{
+			var sut = new NetConfigManager(GetPath("WithWrongConnectionString.config"), null);
+			Assert.Throws<ArgumentException>(() =>
+				sut.GetConnectionString(CONNECTION_NAME));
+		}
 
+		[Test]
+		public void ItFailsIfTheConfigPathWasSpecifiedButCouldntResolveFile()
+		{
+			var sut = new NetConfigManager(GetPath("WithWrongPath.config"), null);
+			Assert.Throws<FileNotFoundException>(() =>
+				sut.GetConnectionString(CONNECTION_NAME));
 		}
 
 		[Test]
@@ -38,6 +50,12 @@ namespace FluentMigrator.Tests.Unit.ConnectionStringName
 
 		[Test]
 		public void ItFailsSilentlyOnMissingAppConfig()
+		{
+
+		}
+
+		[Test]
+		public void ItFailsSilentlyOnMissingAppConfigConnectionString()
 		{
 
 		}
