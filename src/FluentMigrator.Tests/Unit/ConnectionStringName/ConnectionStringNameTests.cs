592d978415112e0b02fd03d997b91f24d337b4e8
tkellogg <tkellogg@kpaonline.com>
2011-08-04 22:26:08 -0600

    Added some test fixtures

--- src/FluentMigrator.Tests/Unit/ConnectionStringName/ConnectionStringNameTests.cs
+++ src/FluentMigrator.Tests/Unit/ConnectionStringName/ConnectionStringNameTests.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using FluentMigrator.Runner.Initialization;
 
 namespace FluentMigrator.Tests.Unit.ConnectionStringName
 {
 	[TestFixture]
 	class ConnectionStringNameTests
 	{
+		private static string GetPath(string relative) 
+		{
+			return string.Format(@"..\..\Unit\ConnectionStringName\Fixtures\{0}", relative);
+		}
+
+		private const string CONNECTION_NAME = "Test.Connection";
+
 		[Test]
 		public void ItFirstTriesConfigPath()
 		{
-
+			var sut = new NetConfigManager(GetPath(""), null);
+			var result = sut.GetConnectionString(CONNECTION_NAME);
 		}
 
 		[Test]
