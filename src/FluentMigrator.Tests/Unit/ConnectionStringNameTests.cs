cd66dd1025e78e978ce3a680e22953e725f2aa93
tkellogg <tkellogg@kpaonline.com>
2011-08-04 21:46:50 -0600

    Added some empty tests for config fallback order

---
+++ src/FluentMigrator.Tests/Unit/ConnectionStringNameTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FluentMigrator.Tests.Unit
+{
+	[TestFixture]
+	class ConnectionStringNameTests
+	{
+		[Test]
+		public void ItFirstTriesConfigPath()
+		{
+
+		}
+
+		[Test]
+		public void ItFailsIfTheConfigPathWasSpecifiedButCouldntResolve()
+		{
+
+		}
+
+		[Test]
+		public void ItTriesAppConfigSecond()
+		{
+
+		}
+
+		[Test]
+		public void ItFailsSilentlyOnMissingAppConfig()
+		{
+
+		}
+
+		// TODO: figure out how to test machine.config settings
+	}
+}
