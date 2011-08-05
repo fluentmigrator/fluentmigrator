0138c574c4b06ecb4715ecd670aa4b1b4b631421
tkellogg <tkellogg@kpaonline.com>
2011-08-04 21:34:10 -0600

    Added a better fallback mechanism for connection string name
    ref #134

--- src/FluentMigrator.Runner/Initialization/NetConfigManager.cs
+++ src/FluentMigrator.Runner/Initialization/NetConfigManager.cs
@@ -10,50 +10,56 @@ namespace FluentMigrator.Runner.Initialization
 	/// </summary>
 	internal class NetConfigManager
 	{
-		private string configPath;
 
-		public NetConfigManager(string configPath)
+		public NetConfigManager(string configPath, string targetAssemblyPath)
 		{
 			ConfigPath = configPath;
+			AssemblyPath = targetAssemblyPath;
 		}
 
-		public string ConfigPath
-		{
-			get
-			{
-				if (!string.IsNullOrEmpty(configPath))
-					return configPath;
-				else
-					// returning null will force the usage of the machine config that is currently in memory
-					return null;
-			}
-			set { configPath = value; }
-		}
+		private string ConfigPath { get; set; }
+		private string AssemblyPath { get; set; }
 
 		public string GetConnectionString(string named)
 		{
-			var path = ConfigPath;
-			if (path != null)
+			if (!string.IsNullOrEmpty(ConfigPath))
 			{
-				if (!File.Exists(path))
-					throw new FileNotFoundException("The config file specified could not be found", path);
+				if (!File.Exists(ConfigPath))
+					throw new FileNotFoundException("The config file specified could not be found", ConfigPath);
 
-				Console.WriteLine(string.Format("Using config '{0}'", path));
-				var c = new XmlDocument();
-				c.Load(path);
-				var xpath = string.Format("//connectionStrings/add[@name='{0}']/@connectionString", named);
-				var attr = c.SelectSingleNode(xpath) as XmlAttribute;
-
-				if (attr != null && !string.IsNullOrEmpty(attr.Value))
-					return attr.Value;
-				else throw new ArgumentException("Could not find connection string named by " + named);
+				var cs = GetConnectionStringFromPath(named, ConfigPath);
+				if (cs != null)
+					return cs;
 			}
-			else
+
+			if (!string.IsNullOrEmpty(AssemblyPath))
 			{
-				var connection = ConfigurationManager.ConnectionStrings[named];
-				return GetConnectionString(named, connection);
-			} 
-			
+				var conf = ConfigurationManager.OpenExeConfiguration(AssemblyPath);
+				if (conf != null && conf.ConnectionStrings != null && conf.ConnectionStrings.ConnectionStrings != null)
+				{
+					var cs = GetConnectionString(named, conf.ConnectionStrings.ConnectionStrings[named]);
+					if (cs != null)
+						return cs;
+				}
+			}
+
+			// if all above failed, just use .NET's native mechanism, which includes `machine.config`
+			var connection = ConfigurationManager.ConnectionStrings[named];
+			return GetConnectionString(named, connection);
+		}
+
+		private static string GetConnectionStringFromPath(string named, string path)
+		{
+			if (!File.Exists(path))
+				return null;
+
+			Console.WriteLine(string.Format("Using config '{0}'", path));
+			var c = new XmlDocument();
+			c.Load(path);
+			var xpath = string.Format("//connectionStrings/add[@name='{0}']/@connectionString", named);
+			var attr = c.SelectSingleNode(xpath) as XmlAttribute;
+
+			return (attr != null && !string.IsNullOrEmpty(attr.Value)) ? attr.Value : null;
 		}
 
 		private static string GetConnectionString(string named, ConnectionStringSettings connection)
