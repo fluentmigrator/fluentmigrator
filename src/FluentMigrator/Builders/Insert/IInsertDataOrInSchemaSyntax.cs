4adbd08bc20426647286708e0d0554432bd71970
abusby <andrew.busby@aimstrategic.com>
2011-08-18 23:37:48 +0100

    Fixed some issue with files missing from the FM runner

---
+++ src/FluentMigrator/Builders/Insert/IInsertDataOrInSchemaSyntax.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentMigrator.Builders.Insert {
+    public interface IInsertDataOrInSchemaSyntax : IInsertDataSyntax {
+        IInsertDataSyntax InSchema(string schemaName);
+    }
+}
