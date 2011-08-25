15f26baf1c3f3b0399482c03e7922045220ad566
Daniel Lee <daniel_lee_limerick@yahoo.com>
2011-08-25 08:49:23 +0200

    Git rm of dummy and powershell files
    Forgot to rm the files in the last commit.

--- packages/FluentMigrator.Tools/Install.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$project.ProjectItems | ForEach { if ($_.Name -eq "InstallationDummyFile.txt") { $_.Remove() } }
-$projectPath = Split-Path $project.FullName -Parent
-Join-Path $projectPath "InstallationDummyFile.txt" | Remove-Item
