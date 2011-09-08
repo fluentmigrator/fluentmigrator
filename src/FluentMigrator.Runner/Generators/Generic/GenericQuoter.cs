ad198320f8c28b41750f9d6401e129d5f631b998
Tabushi <evgeny.akhtimirov@gmail.com>
2011-09-08 11:13:14 +0600

    [!] move to_date() from method GenericQuoter.FormatDateTime to method FormatDateTime.FormatDateTime;

--- src/FluentMigrator.Runner/Generators/Generic/GenericQuoter.cs
+++ src/FluentMigrator.Runner/Generators/Generic/GenericQuoter.cs
@@ -89,7 +89,7 @@ namespace FluentMigrator.Runner.Generators.Generic
 
         public virtual string FormatDateTime(DateTime value)
         {
-            var result = string.Format("to_date({0}{1}{0}, {0}yyyy-mm-dd hh24:mi:ss{0})", ValueQuote, value.ToString("yyyy-MM-dd HH:mm:ss")); //ISO 8601 DATETIME FORMAT (EXCEPT 'T' CHAR)
+            var result = ValueQuote + (value).ToString("yyyy-MM-ddTHH:mm:ss") + ValueQuote;
             return result;
         }
 
--- src/FluentMigrator.Runner/Generators/Oracle/OracleQuoter.cs
+++ src/FluentMigrator.Runner/Generators/Oracle/OracleQuoter.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using FluentMigrator.Runner.Generators.Generic;
 
 namespace FluentMigrator.Runner.Generators.Oracle
 {
     public class OracleQuoter : GenericQuoter
     {
-        //Not what to do here. Orcale quotes are " but if we do that then the column and table names become case
+        public override string FormatDateTime(DateTime value)
+        {
+            var result = string.Format("to_date({0}{1}{0}, {0}yyyy-mm-dd hh24:mi:ss{0})", ValueQuote, value.ToString("yyyy-MM-dd HH:mm:ss")); //ISO 8601 DATETIME FORMAT (EXCEPT 'T' CHAR)
+            return result;
+        }
+
+        //Not what to do here. Oracle quotes are " but if we do that then the column and table names become case
         //sensitive. For now we will just set it to not quote
         public override string OpenQuote { get { return string.Empty; } }
 
         public override string CloseQuote { get { return string.Empty; } }
-
-        
     }
 }
