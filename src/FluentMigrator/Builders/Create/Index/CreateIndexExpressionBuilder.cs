bbc8cf9790f3203375a3dc1b34ddbd6a5c1dfb02
Tabushi <evgeny.akhtimirov@gmail.com>
2011-09-19 00:59:19 +0600

    [+] the ability to create a unique index on the column;

--- src/FluentMigrator/Builders/Create/Index/CreateIndexExpressionBuilder.cs
+++ src/FluentMigrator/Builders/Create/Index/CreateIndexExpressionBuilder.cs
@@ -21,71 +21,77 @@ using FluentMigrator.Model;
 
 namespace FluentMigrator.Builders.Create.Index
 {
-	public class CreateIndexExpressionBuilder : ExpressionBuilderBase<CreateIndexExpression>,
-		ICreateIndexForTableSyntax,
-		ICreateIndexOnColumnOrInSchemaSyntax,
-		ICreateIndexColumnOptionsSyntax,
-		ICreateIndexOptionsSyntax
-	{
-		public IndexColumnDefinition CurrentColumn { get; set; }
+    public class CreateIndexExpressionBuilder : ExpressionBuilderBase<CreateIndexExpression>,
+        ICreateIndexForTableSyntax,
+        ICreateIndexOnColumnOrInSchemaSyntax,
+        ICreateIndexColumnOptionsSyntax,
+        ICreateIndexOptionsSyntax
+    {
+        public IndexColumnDefinition CurrentColumn { get; set; }
 
-		public CreateIndexExpressionBuilder(CreateIndexExpression expression)
-			: base(expression)
-		{
-		}
+        public CreateIndexExpressionBuilder(CreateIndexExpression expression)
+            : base(expression)
+        {
+        }
 
-		public ICreateIndexOnColumnOrInSchemaSyntax OnTable(string tableName)
-		{
-			Expression.Index.TableName = tableName;
-			return this;
-		}
+        public ICreateIndexOnColumnOrInSchemaSyntax OnTable(string tableName)
+        {
+            Expression.Index.TableName = tableName;
+            return this;
+        }
 
-		public ICreateIndexColumnOptionsSyntax OnColumn(string columnName)
-		{
-			CurrentColumn = new IndexColumnDefinition { Name = columnName };
-			Expression.Index.Columns.Add(CurrentColumn);
-			return this;
-		}
+        public ICreateIndexColumnOptionsSyntax OnColumn(string columnName)
+        {
+            CurrentColumn = new IndexColumnDefinition { Name = columnName };
+            Expression.Index.Columns.Add(CurrentColumn);
+            return this;
+        }
 
-		public ICreateIndexOptionsSyntax WithOptions()
-		{
-			return this;
-		}
+        public ICreateIndexOptionsSyntax WithOptions()
+        {
+            return this;
+        }
 
-		public ICreateIndexOnColumnSyntax InSchema(string schemaName)
-		{
-			Expression.Index.SchemaName = schemaName;
-			return this;
-		}
+        public ICreateIndexOnColumnSyntax InSchema(string schemaName)
+        {
+            Expression.Index.SchemaName = schemaName;
+            return this;
+        }
 
-		public ICreateIndexOnColumnSyntax Ascending()
-		{
-			CurrentColumn.Direction = Direction.Ascending;
-			return this;
-		}
+        public ICreateIndexOnColumnSyntax Ascending()
+        {
+            CurrentColumn.Direction = Direction.Ascending;
+            return this;
+        }
 
-		public ICreateIndexOnColumnSyntax Descending()
-		{
-			CurrentColumn.Direction = Direction.Descending;
-			return this;
-		}
+        public ICreateIndexOnColumnSyntax Descending()
+        {
+            CurrentColumn.Direction = Direction.Descending;
+            return this;
+        }
 
-		public ICreateIndexOptionsSyntax Unique()
-		{
-			Expression.Index.IsUnique = true;
-			return this;
-		}
+        ICreateIndexOnColumnSyntax ICreateIndexColumnOptionsSyntax.Unique()
+        {
+            Expression.Index.IsUnique = true;
+            return this;
+        }
 
-		public ICreateIndexOptionsSyntax NonClustered()
-		{
-			Expression.Index.IsClustered = false;
-			return this;
-		}
+        public ICreateIndexOptionsSyntax Unique()
+        {
+            Expression.Index.IsUnique = true;
+            return this;
+        }
 
-		public ICreateIndexOptionsSyntax Clustered()
-		{
-			Expression.Index.IsClustered = true;
-			return this;
-		}
-	}
+        public ICreateIndexOptionsSyntax NonClustered()
+        {
+            Expression.Index.IsClustered = false;
+            return this;
+        }
+
+        public ICreateIndexOptionsSyntax Clustered()
+        {
+            Expression.Index.IsClustered = true;
+            return this;
+        }
+    }
 }
