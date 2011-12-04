51703bb683171fb7a8f6713e8a3b4b58d79ebddf
unknown <Tom@acerLaila.(none)>
2011-12-04 11:50:54 +0100

    Clean up usings

--- src/FluentMigrator.Runner/Initialization/ConnectionStringManager.cs
+++ src/FluentMigrator.Runner/Initialization/ConnectionStringManager.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Configuration;
-using System.IO;
 using System.Linq;
 using FluentMigrator.Runner.Processors;
 
--- src/FluentMigrator.Tests/Unit/Initialization/ConnectionStringManagerTests.cs
+++ src/FluentMigrator.Tests/Unit/Initialization/ConnectionStringManagerTests.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using FluentMigrator.Runner;
+﻿using FluentMigrator.Runner;
 using NUnit.Framework;
 using FluentMigrator.Runner.Initialization;
-using System.IO;
 using Moq;
 using System.Configuration;
-using System.Reflection;
 
 namespace FluentMigrator.Tests.Unit.Initialization
 {
