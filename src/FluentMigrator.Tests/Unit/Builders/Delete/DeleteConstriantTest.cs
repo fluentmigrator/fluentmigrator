d2e9f926e90dc33d66c9c5ec14e04c97368ce812
Andrew Busby <andrew.busby@aimstrategic.com>
2011-12-19 18:41:28 +0000

    Updated delete expressions to include Delete.PrimaryKey()

---
+++ src/FluentMigrator.Tests/Unit/Builders/Delete/DeleteConstriantTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FluentMigrator.Tests.Unit.Builders.Delete
+{
+    [TestFixture]
+    public class DeleteConstriantTest
+    {
+        [Test]
+        public void NoTestForDeleteConstraint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
