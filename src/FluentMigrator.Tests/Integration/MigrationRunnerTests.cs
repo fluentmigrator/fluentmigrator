e56a64310543b9a72413f9dae7df3eee6029e5f3
Alex Ullrich <alex.ullrich@gmail.com>
2012-07-04 21:22:34 -0400

    add sample migration for update.AllRows syntax

--- src/FluentMigrator.Tests/Integration/MigrationRunnerTests.cs
+++ src/FluentMigrator.Tests/Integration/MigrationRunnerTests.cs
@@ -376,7 +376,8 @@ namespace FluentMigrator.Tests.Integration
                 runner.VersionLoader.VersionInfo.HasAppliedMigration(1).ShouldBeTrue();
                 runner.VersionLoader.VersionInfo.HasAppliedMigration(2).ShouldBeTrue();
                 runner.VersionLoader.VersionInfo.HasAppliedMigration(3).ShouldBeTrue();
-                runner.VersionLoader.VersionInfo.Latest().ShouldBe(3);
+                runner.VersionLoader.VersionInfo.HasAppliedMigration(4).ShouldBeTrue();
+                runner.VersionLoader.VersionInfo.Latest().ShouldBe(4);
 
                 runner.RollbackToVersion(0, false);
             });
