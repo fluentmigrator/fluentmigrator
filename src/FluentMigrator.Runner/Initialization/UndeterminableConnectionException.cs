1a037df1f01810f0e36487ff473d150d326df40f
Biegal <marcin@biegala.net>
2012-09-26 09:27:31 +0200

    Marked UndeterminableConnectionException as serializable, added standard constructors. #299

--- src/FluentMigrator.Runner/Initialization/UndeterminableConnectionException.cs
+++ src/FluentMigrator.Runner/Initialization/UndeterminableConnectionException.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace FluentMigrator.Runner.Initialization
 {
+    [Serializable]
     public class UndeterminableConnectionException : ApplicationException
     {
+        public UndeterminableConnectionException() { }
+
         public UndeterminableConnectionException(string message) : base(message) { }
+
+        public UndeterminableConnectionException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected UndeterminableConnectionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
