e131b8757aa32fb76fe910dc2d002c3fadb4f64f
Daniel Lee <daniel_lee_limerick@yahoo.com>
2012-09-30 23:41:16 +0200

    Add Create and Drop Sequence to Sql Server 2012 Generator
    New test class for SqlServer2012Generator and can generate sql for the
    CreateSequenceExpression and DeleteSequenceExpression.

--- src/FluentMigrator.Runner/Generators/SqlServer/SqlServer2012Generator.cs
+++ src/FluentMigrator.Runner/Generators/SqlServer/SqlServer2012Generator.cs
@@ -1,10 +1,59 @@
-﻿namespace FluentMigrator.Runner.Generators.SqlServer
+﻿using System.Text;
+
+namespace FluentMigrator.Runner.Generators.SqlServer
 {
     public class SqlServer2012Generator : SqlServer2008Generator
     {
         public SqlServer2012Generator()
             :base(new SqlServerColumn(new SqlServer2008TypeMap()))
         {
+        }
+
+        public override string Generate(Expressions.CreateSequenceExpression expression)
+        {
+            var result = new StringBuilder(string.Format("CREATE SEQUENCE "));
+            var seq = expression.Sequence;
+            result.AppendFormat("{0}.{1}", Quoter.QuoteSchemaName(seq.SchemaName), Quoter.QuoteSequenceName(seq.Name));
+
+            if (seq.Increment.HasValue)
+            {
+                result.AppendFormat(" INCREMENT BY {0}", seq.Increment);
+            }
+
+            if (seq.MinValue.HasValue)
+            {
+                result.AppendFormat(" MINVALUE {0}", seq.MinValue);
+            }
+
+            if (seq.MaxValue.HasValue)
+            {
+                result.AppendFormat(" MAXVALUE {0}", seq.MaxValue);
+            }
+
+            if (seq.StartWith.HasValue)
+            {
+                result.AppendFormat(" START WITH {0}", seq.StartWith);
+            }
+
+            if (seq.Cache.HasValue)
+            {
+                result.AppendFormat(" CACHE {0}", seq.Cache);
+            }
+
+            if (seq.Cycle)
+            {
+                result.Append(" CYCLE");
+            }
+
+            return result.ToString();
+        }
+
+        public override string Generate(Expressions.DeleteSequenceExpression expression)
+        {
+            var result = new StringBuilder(string.Format("DROP SEQUENCE "));
+            result.AppendFormat("{0}.{1}", Quoter.QuoteSchemaName(expression.SchemaName), Quoter.QuoteSequenceName(expression.SequenceName));
+
+            return result.ToString();
         }
     }
 }
---
+++ src/FluentMigrator.Tests/Unit/Generators/SqlServer2012/SqlServer2012GeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentMigrator.Expressions;
+using FluentMigrator.Runner.Generators.SqlServer;
+using NUnit.Framework;
+using NUnit.Should;
+
+namespace FluentMigrator.Tests.Unit.Generators.SqlServer2012
+{
+    [TestFixture]
+    public class SqlServer2012GeneratorTests
+    {
+    
+        private IMigrationGenerator generator;
+
+        [SetUp]
+        public void Setup()
+        {
+            generator = new SqlServer2012Generator();
+        }
+
+
+        [Test]
+        public void CanCreateSequence()
+        {
+            var expression = new CreateSequenceExpression
+            {
+                Sequence =
+                {
+                    Cache = 10,
+                    Cycle = true,
+                    Increment = 2,
+                    MaxValue = 100,
+                    MinValue = 0,
+                    Name = "Sequence",
+                    SchemaName = "TestSchema",
+                    StartWith = 2
+                }
+            };
+            var sql = generator.Generate(expression);
+            sql.ShouldBe("CREATE SEQUENCE [TestSchema].[Sequence] INCREMENT BY 2 MINVALUE 0 MAXVALUE 100 START WITH 2 CACHE 10 CYCLE");
+        }
+
+        [Test]
+        public void CanDeleteSequence()
+        {
+            var expression = new DeleteSequenceExpression { SchemaName = "TestSchema", SequenceName = "Sequence" };
+            var sql = generator.Generate(expression);
+            sql.ShouldBe("DROP SEQUENCE [TestSchema].[Sequence]");
+        }
+    }
+}
