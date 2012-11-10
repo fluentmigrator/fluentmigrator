98f3810a4276caa0bef6adf95499288b7f0a6398
Tom Marien <tommarien@gmail.com>
2012-11-10 13:42:07 +0100

    Correct namespace of DotConnnectOracleDbFactory

--- src/FluentMigrator.Runner/Processors/DotConnectOracle/DotConnectOracleDbFactory.cs
+++ src/FluentMigrator.Runner/Processors/DotConnectOracle/DotConnectOracleDbFactory.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-namespace FluentMigrator.Runner.Processors.Oracle
+namespace FluentMigrator.Runner.Processors.DotConnectOracle
 {
    
     public class DotConnectOracleDbFactory : ReflectionBasedDbFactory
