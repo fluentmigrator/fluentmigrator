cec5dadd77891bef1463a763cb5d48296dfb62ae
Tom Marien <tommarien@gmail.com>
2012-12-31 12:49:55 +0100

    Packaging 1.0.6

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FluentMigrator")]
 [assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2012")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 
 [assembly: AssemblyConfigurationAttribute("Debug")]
