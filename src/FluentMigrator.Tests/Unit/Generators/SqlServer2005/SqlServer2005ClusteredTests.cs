00a23b44b48ee9d730f7136a54efd40fb7464681
Matthew Dunsdon <matthew.dunsdon@bluewire-technologies.com>
2013-05-24 12:19:54 +0100

    Adds generator SQL Server clustering unit tests

--- src/FluentMigrator.Tests/Unit/Generators/SqlServer2005/SqlServer2005ClusteredTests.cs
+++ src/FluentMigrator.Tests/Unit/Generators/SqlServer2005/SqlServer2005ClusteredTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentMigrator.Runner.Generators.SqlServer;
+﻿using FluentMigrator.Runner.Extensions;
+using FluentMigrator.Runner.Generators.SqlServer;
 using NUnit.Framework;
 using NUnit.Should;
 
@@ -55,6 +56,174 @@ namespace FluentMigrator.Tests.Unit.Generators.SqlServer2005
 
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE CLUSTERED INDEX [TestIndex] ON [dbo].[TestTable1] ([TestColumn1] ASC, [TestColumn2] DESC)");
+        }
+
+        [Test]
+        public void CanCreateNamedClusteredPrimaryKeyConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY CLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedClusteredPrimaryKeyConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY CLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedClusteredUniqueConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE CLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedClusteredUniqueConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE CLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnClusteredPrimaryKeyConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY CLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnClusteredPrimaryKeyConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY CLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnClusteredUniqueConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE CLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnClusteredUniqueConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.Clustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE CLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnNonClusteredPrimaryKeyConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY NONCLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnNonClusteredPrimaryKeyConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY NONCLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnNonClusteredUniqueConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE NONCLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedMultiColumnNonClusteredUniqueConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedMultiColumnUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE NONCLUSTERED ([TestColumn1], [TestColumn2])");
+        }
+
+        [Test]
+        public void CanCreateNamedNonClusteredPrimaryKeyConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY NONCLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedNonClusteredPrimaryKeyConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedPrimaryKeyExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTPRIMARYKEY] PRIMARY KEY NONCLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedNonClusteredUniqueConstraintWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+            expression.Constraint.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [TestSchema].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE NONCLUSTERED ([TestColumn1])");
+        }
+
+        [Test]
+        public void CanCreateNamedNonClusteredUniqueConstraintWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateNamedUniqueConstraintExpression();
+            expression.Constraint.AdditionalFeatures.Add(SqlServerExtensions.ConstraintType, SqlServerConstraintType.NonClustered);
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [TESTUNIQUECONSTRAINT] UNIQUE NONCLUSTERED ([TestColumn1])");
         }
 
         [Test]
