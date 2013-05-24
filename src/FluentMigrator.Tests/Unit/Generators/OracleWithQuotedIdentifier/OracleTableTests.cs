e7a7cb68cf1a6342d8395f09c01d2355a0df7bad
Matthew Dunsdon <matthew.dunsdon@bluewire-technologies.com>
2013-05-24 08:06:56 +0100

    Adds generator table unit tests

--- src/FluentMigrator.Tests/Unit/Generators/OracleWithQuotedIdentifier/OracleTableTests.cs
+++ src/FluentMigrator.Tests/Unit/Generators/OracleWithQuotedIdentifier/OracleTableTests.cs
@@ -17,9 +17,38 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
         }
 
         [Test]
+        public void CanCreateTableWithCustomColumnTypeWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableExpression();
+            expression.Columns[0].IsPrimaryKey = true;
+            expression.Columns[1].Type = null;
+            expression.Columns[1].CustomType = "BINARY_DOUBLE";
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" BINARY_DOUBLE NOT NULL, PRIMARY KEY (\"TestColumn1\"))");
+        }
+
+        [Test]
         public void CanCreateTableWithCustomColumnTypeWithDefaultSchema()
         {
-            //Do not knows any oracle custom types. Not testing for it.  If someone else knows please add a test
+            var expression = GeneratorTestHelper.GetCreateTableExpression();
+            expression.Columns[0].IsPrimaryKey = true;
+            expression.Columns[1].Type = null;
+            expression.Columns[1].CustomType = "BINARY_DOUBLE";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" BINARY_DOUBLE NOT NULL, PRIMARY KEY (\"TestColumn1\"))");
+        }
+
+        [Test]
+        public void CanCreateTableWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL)");
         }
 
         [Test]
@@ -32,9 +61,36 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
         }
 
         [Test]
+        public void CanCreateTableWithDefaultValueExplicitlySetToNullWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableExpression();
+            expression.Columns[0].DefaultValue = null;
+            expression.Columns[0].TableName = expression.TableName;
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) DEFAULT NULL NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL)");
+        }
+
+        [Test]
         public void CanCreateTableWithDefaultValueExplicitlySetToNullWithDefaultSchema()
         {
-            //Not sure how this would work in oracle.  Someone please add a test
+            var expression = GeneratorTestHelper.GetCreateTableExpression();
+            expression.Columns[0].DefaultValue = null;
+            expression.Columns[0].TableName = expression.TableName;
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) DEFAULT NULL NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL)");
+        }
+
+        [Test]
+        public void CanCreateTableWithDefaultValueWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithDefaultValue();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) DEFAULT 'Default' NOT NULL, \"TestColumn2\" NUMBER(10,0) DEFAULT 0 NOT NULL)");
         }
 
         [Test]
@@ -42,10 +98,17 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
         {
             var expression = GeneratorTestHelper.GetCreateTableWithDefaultValue();
 
-            // Oracle requires the DEFAULT clause to appear before the NOT NULL clause
-
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) DEFAULT 'Default' NOT NULL, \"TestColumn2\" NUMBER(10,0) DEFAULT 0 NOT NULL)");
+        }
+
+        [Test]
+        public void CanCreateTableWithIdentityWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithAutoIncrementExpression();
+            expression.SchemaName = "TestSchema";
+
+            Assert.Throws<DatabaseOperationNotSupportedException>(() => Generator.Generate(expression));
         }
 
         [Test]
@@ -55,14 +118,32 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
         }
 
         [Test]
+        public void CanCreateTableWithMultiColumnPrimaryKeyWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithMultiColumnPrimaryKeyExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, PRIMARY KEY (\"TestColumn1\", \"TestColumn2\"))");
+        }
+
+        [Test]
         public void CanCreateTableWithMultiColumnPrimaryKeyWithDefaultSchema()
         {
             var expression = GeneratorTestHelper.GetCreateTableWithMultiColumnPrimaryKeyExpression();
 
-            // See the note in OracleColumn about why the PK should not be named
-
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, PRIMARY KEY (\"TestColumn1\", \"TestColumn2\"))");
+        }
+
+        [Test]
+        public void CanCreateTableWithNamedMultiColumnPrimaryKeyWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithNamedMultiColumnPrimaryKeyExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, CONSTRAINT \"TestKey\" PRIMARY KEY (\"TestColumn1\", \"TestColumn2\"))");
         }
 
         [Test]
@@ -72,6 +153,16 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
 
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, CONSTRAINT \"TestKey\" PRIMARY KEY (\"TestColumn1\", \"TestColumn2\"))");
+        }
+
+        [Test]
+        public void CanCreateTableWithNamedPrimaryKeyWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithNamedPrimaryKeyExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, CONSTRAINT \"TestKey\" PRIMARY KEY (\"TestColumn1\"))");
         }
 
         [Test]
@@ -87,20 +178,48 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
         public void CanCreateTableWithNullableFieldWithCustomSchema()
         {
             var expression = GeneratorTestHelper.GetCreateTableWithNullableColumn();
+            expression.SchemaName = "TestSchema";
 
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255), \"TestColumn2\" NUMBER(10,0) NOT NULL)");
         }
 
         [Test]
+        public void CanCreateTableWithNullableFieldWithDefaultSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithNullableColumn();
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255), \"TestColumn2\" NUMBER(10,0) NOT NULL)");
+        }
+
+        [Test]
+        public void CanCreateTableWithPrimaryKeyWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetCreateTableWithPrimaryKeyExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, PRIMARY KEY (\"TestColumn1\"))");
+        }
+
+        [Test]
         public void CanCreateTableWithPrimaryKeyWithDefaultSchema()
         {
-            //After a quick goolge the below tested statment should work.
-            //CONSTRAINT constraint_name PRIMARY KEY (column1, column2, . column_n)
             var expression = GeneratorTestHelper.GetCreateTableWithPrimaryKeyExpression();
 
             var result = Generator.Generate(expression);
             result.ShouldBe("CREATE TABLE \"TestTable1\" (\"TestColumn1\" NVARCHAR2(255) NOT NULL, \"TestColumn2\" NUMBER(10,0) NOT NULL, PRIMARY KEY (\"TestColumn1\"))");
+        }
+
+        [Test]
+        public void CanDropTableWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetDeleteTableExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("DROP TABLE \"TestTable1\"");
         }
 
         [Test]
@@ -110,6 +229,16 @@ namespace FluentMigrator.Tests.Unit.Generators.OracleWithQuotedIdentifier
 
             var result = Generator.Generate(expression);
             result.ShouldBe("DROP TABLE \"TestTable1\"");
+        }
+
+        [Test]
+        public void CanRenameTableWithCustomSchema()
+        {
+            var expression = GeneratorTestHelper.GetRenameTableExpression();
+            expression.SchemaName = "TestSchema";
+
+            var result = Generator.Generate(expression);
+            result.ShouldBe("ALTER TABLE \"TestTable1\" RENAME TO \"TestTable2\"");
         }
 
         [Test]
