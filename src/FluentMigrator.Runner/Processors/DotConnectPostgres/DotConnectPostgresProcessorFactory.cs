7e090e57ab46a3a5ccfa413446252eafb7ea720d
Rafael Teno Dias <rfteno@gmail.com>
2013-07-20 11:49:09 -0400

    Add DotConnectPostgres

---
+++ src/FluentMigrator.Runner/Processors/DotConnectPostgres/DotConnectPostgresProcessorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentMigrator.Runner.Processors.DotConnectPostgres
+{
+    using Generators.Postgres;
+
+    public class DotConnectPostgresProcessorFactory : MigrationProcessorFactory
+    {
+        public override IMigrationProcessor Create(string connectionString, IAnnouncer announcer, IMigrationProcessorOptions options)
+        {
+            var factory = new DotConnectPostgresDbFactory();
+            var connection = factory.CreateConnection(connectionString);
+            return new DotConnectPostgresProcessor(connection, new PostgresGenerator(), announcer, options, factory);
+        }
+    }
+}
