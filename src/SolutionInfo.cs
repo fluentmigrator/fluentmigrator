6e79d0fdddd952f196c80f12af33e90837b35de7
cjundt <cjundt@sinalys.com>
2014-04-16 16:14:39 +0200

    Oracle Managed Data Access
    Introducing new processor (derived from Oracle processor) to handle
    Oracle's .NET 4+ managed client connector.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FluentMigrator")]
 [assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2014")]
-[assembly: AssemblyVersion("1.1.2.1")]
-[assembly: AssemblyFileVersion("1.1.2.1")]
+[assembly: AssemblyVersion("1.1.2.99")]
 
 [assembly: AssemblyConfigurationAttribute("Debug")]
 [assembly: System.CLSCompliantAttribute(true)]
