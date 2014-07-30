95ec96bc5b437d4c5e22aac9badf0d84a5364168
Inurion <inurion@live.com>
2014-07-30 10:38:39 -0300

    Method UniqueConstraint now accepts params string[] instead of string [] Closes #477

--- src/FluentMigrator/Builders/Create/Constraint/ICreateConstraintColumnsSyntax.cs
+++ src/FluentMigrator/Builders/Create/Constraint/ICreateConstraintColumnsSyntax.cs
@@ -3,6 +3,6 @@ namespace FluentMigrator.Builders.Create.Constraint
     public interface ICreateConstraintColumnsSyntax
     {
         ICreateConstraintOptionsSyntax Column(string columnName);
-        ICreateConstraintOptionsSyntax Columns(string[] columnNames);
+        ICreateConstraintOptionsSyntax Columns(params string[] columnNames);
     }
 }
