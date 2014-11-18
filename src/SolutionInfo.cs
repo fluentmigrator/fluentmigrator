0a850ca8d904326232316734738914f59496ffda
Tom Marien <tommarien@gmail.com>
2014-11-18 11:14:42 +0100

    Bumped version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FluentMigrator")]
 [assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2014")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 
 [assembly: AssemblyConfigurationAttribute("Debug")]
 [assembly: System.CLSCompliantAttribute(true)]
