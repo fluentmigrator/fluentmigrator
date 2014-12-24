d56d8a7a4ae91303ef758e16099787c8bd30d816
Tom Marien <tommarien@gmail.com>
2014-12-24 14:07:20 +0100

    Revert changes to IntegrationTestOptions

--- src/FluentMigrator.Tests/IntegrationTestOptions.cs
+++ src/FluentMigrator.Tests/IntegrationTestOptions.cs
@@ -19,7 +19,7 @@ namespace FluentMigrator.Tests
         public static DatabaseServerOptions SqlServer2012 = new DatabaseServerOptions
         {
             ConnectionString = @"server=.\SQLEXPRESS;uid=test;pwd=test;Trusted_Connection=yes;database=FluentMigrator",
-            IsEnabled = true
+            IsEnabled = false 
         };
 
         public static DatabaseServerOptions SqlServerCe = new DatabaseServerOptions
@@ -48,8 +48,8 @@ namespace FluentMigrator.Tests
 
         public static DatabaseServerOptions Postgres = new DatabaseServerOptions
             {
-                ConnectionString = "Server=127.0.0.1;Port=5432;Database=fluentmigrator;User Id=test;Password=test;",
-                IsEnabled = true
+                ConnectionString = "Server=127.0.0.1;Port=5432;Database=FluentMigrator;User Id=test;Password=test;",
+                IsEnabled = false
             };
 
         public static DatabaseServerOptions Firebird = new DatabaseServerOptions
