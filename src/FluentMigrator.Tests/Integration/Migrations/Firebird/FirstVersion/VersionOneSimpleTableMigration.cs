c3f05722adeadecb9acd340b92df143cf39cd4b0
Amro El-Fakharany <amro00765@outlook.com>
2015-02-06 08:33:56 +0100

    add an end-to-end test proofing that rolling back an initial migration fails to drop VersionInfo table

---
+++ src/FluentMigrator.Tests/Integration/Migrations/Firebird/FirstVersion/VersionOneSimpleTableMigration.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FluentMigrator.Tests.Integration.Migrations.Firebird.FirstVersion
+{
+    [Migration(1)]
+    public class VersionOneSimpleTableMigration : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("SIMPLE")
+                .WithColumn("ID").AsInt32().PrimaryKey()
+                .WithColumn("COL_STR").AsString(10);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("SIMPLE");
+        }
+    }
+}
