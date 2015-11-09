cac48b707dff2a8d830cd7381f7df9197f9ca35c
Tom Marien <tommarien@gmail.com>
2015-11-09 20:46:05 +0100

    Bump patch to 1.6.1

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
 [assembly: AssemblyProduct("FluentMigrator")]
 [assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2015")]
 [assembly: AssemblyConfigurationAttribute("Debug")]
