f17dca3ba0ada883bf016d7bf297c9e0b8b8bdf7
Mark Junker <m.junker@fubar-dev.de>
2016-05-31 10:37:45 +0200

    Bumped version and copyright

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
 [assembly: AssemblyProduct("FluentMigrator")]
-[assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2015")]
+[assembly: AssemblyCopyright("Copyright - Sean Chambers 2008-2016")]
 [assembly: AssemblyConfigurationAttribute("Debug")]
 [assembly: CLSCompliant(true)]
