2d0221b88b8a3433925b1cd7716f08aa24e746c1
Jean-Christophe Berleur <jc@warrantymaster.com>
2016-11-17 13:03:39 -0500

    add description support for mysql

--- src/FluentMigrator.Runner/Generators/MySql/MySqlColumn.cs
+++ src/FluentMigrator.Runner/Generators/MySql/MySqlColumn.cs
@@ -9,6 +9,14 @@ namespace FluentMigrator.Runner.Generators.MySql
         public MySqlColumn()
             : base(new MySqlTypeMap(), new MySqlQuoter())
         {
+            ClauseOrder.Add(FormatDescription);
+        }
+
+        protected string FormatDescription(ColumnDefinition column)
+        {
+            return string.IsNullOrEmpty(column.ColumnDescription)
+                ? string.Empty
+                : string.Format("COMMENT {0}", Quoter.QuoteValue(column.ColumnDescription));
         }
 
         protected override string FormatIdentity(ColumnDefinition column)
