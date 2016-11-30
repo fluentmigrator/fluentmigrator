1bc3ef92de21be69b497da6757c75c7ec89162f5
Eric Burcham <eburcham@eprod.com>
2016-11-30 12:11:29 -0600

    Removed the usage of the nameof keyword, which the Travis-CI build also does not support.

--- src/FluentMigrator.Tests/Integration/IntegrationTestBase.cs
+++ src/FluentMigrator.Tests/Integration/IntegrationTestBase.cs
@@ -57,7 +57,7 @@ namespace FluentMigrator.Tests.Integration
 
             if (IntegrationTestOptions.AnyServerTypesEnabled == false)
             {
-                var optionsClassName = nameof(IntegrationTestOptions);
+                var optionsClassName = typeof(IntegrationTestOptions).Name;
                 Assert.Fail("No database processors are configured to run your migration tests.  This message is provided to avoid false positives.  To avoid this message enable one or more test runners in the {0} class.", optionsClassName);
             }
 
