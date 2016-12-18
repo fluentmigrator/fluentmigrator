bb407616086adeac4bd8a57ce7e87d7f1aa42911
Eivind Gussiås Løkseth <eloekset@msn.com>
2016-12-18 13:06:42 +0100

    Add Generator and ProcessorFactory for SqlServer2016.

---
+++ src/FluentMigrator.Runner/Generators/SqlServer/SqlServer2016Generator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentMigrator.Runner.Generators.SqlServer
+{
+    public class SqlServer2016Generator : SqlServer2014Generator
+    {
+
+    }
+}
