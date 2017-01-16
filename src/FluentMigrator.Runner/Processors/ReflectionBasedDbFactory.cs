ca04ca83214ee32b6e402939519953c375955f19
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2017-01-16 11:22:41 +0300

    use Microsoft.Data.Sqlite in COREFX as System.Data.SQLite is not available yet (even though it doesn't work yet as well)

--- src/FluentMigrator.Runner/Processors/ReflectionBasedDbFactory.cs
+++ src/FluentMigrator.Runner/Processors/ReflectionBasedDbFactory.cs
@@ -4,6 +4,7 @@ namespace FluentMigrator.Runner.Processors
     using System.Data.Common;
     using System.Reflection;
 #if COREFX
+    using System.Linq;
     using System.Runtime.Loader;
 #endif
 
@@ -23,6 +24,19 @@ namespace FluentMigrator.Runner.Processors
 #if COREFX
             var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(assemblyName));
             var type = assembly.GetType(dbProviderFactoryTypeName);
+
+            var ctors = type.GetConstructors();
+            if (!ctors.Any(x => x.GetParameters().Length == 0))
+            {
+                var instanceField = type.GetField("Instance", BindingFlags.Static | BindingFlags.Public);
+                if (instanceField != null)
+                    return (DbProviderFactory)instanceField.GetValue(null);
+
+                var instanceProperty = type.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public);
+                if (instanceProperty != null)
+                    return (DbProviderFactory)instanceProperty.GetValue(null);
+            }
+
             return (DbProviderFactory)Activator.CreateInstance(type);
 #else
             return (DbProviderFactory)AppDomain.CurrentDomain.CreateInstanceAndUnwrap(assemblyName, dbProviderFactoryTypeName);
--- src/FluentMigrator.Runner/Processors/SQLite/SQLiteDbFactory.cs
+++ src/FluentMigrator.Runner/Processors/SQLite/SQLiteDbFactory.cs
@@ -3,7 +3,11 @@ namespace FluentMigrator.Runner.Processors.SQLite
     public class SQLiteDbFactory : ReflectionBasedDbFactory
     {
         public SQLiteDbFactory()
+#if COREFX
+            : base("Microsoft.Data.Sqlite", "Microsoft.Data.Sqlite.SqliteFactory")
+#else
             : base("System.Data.SQLite", "System.Data.SQLite.SQLiteFactory")
+#endif
         {
         }
     }
