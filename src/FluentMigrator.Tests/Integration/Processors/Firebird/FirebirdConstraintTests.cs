f05199fc7ab994240217c87a0ea2e0fbdc3db1b0
amroel <amro00765@gmail.com>
2017-03-02 18:00:28 +0100

    refactored to use the new infrastructure

--- src/FluentMigrator.Tests/Integration/Processors/Firebird/FirebirdConstraintTests.cs
+++ src/FluentMigrator.Tests/Integration/Processors/Firebird/FirebirdConstraintTests.cs
@@ -1,7 +1,4 @@
-﻿using FirebirdSql.Data.FirebirdClient;
-using FluentMigrator.Runner.Announcers;
-using FluentMigrator.Runner.Generators.Firebird;
-using FluentMigrator.Runner.Processors;
+﻿using System;
 using FluentMigrator.Runner.Processors.Firebird;
 using NUnit.Framework;
 using NUnit.Should;
@@ -13,83 +10,90 @@ namespace FluentMigrator.Tests.Integration.Processors.Firebird
     [Category("Firebird")]
     public class FirebirdConstraintTests : BaseConstraintTests
     {
-        public FbConnection Connection { get; set; }
-        public FirebirdProcessor Processor { get; set; }
-
-        [SetUp]
-        public void SetUp()
-        {
-            FbDatabase.CreateDatabase(IntegrationTestOptions.Firebird.ConnectionString);
-
-            Connection = new FbConnection(IntegrationTestOptions.Firebird.ConnectionString);
-            var options = FirebirdOptions.AutoCommitBehaviour();
-            Processor = new FirebirdProcessor(Connection, new FirebirdGenerator(options), new TextWriterAnnouncer(System.Console.Out), new ProcessorOptions(), new FirebirdDbFactory(), options);
-            Connection.Open();
-            Processor.BeginTransaction();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            if (!Processor.WasCommitted)
-                Processor.CommitTransaction();
-            Connection.Close();
-
-            FbDatabase.DropDatabase(IntegrationTestOptions.Firebird.ConnectionString);
-        }
+        private static readonly Type FB_PROCESSOR = typeof(FirebirdProcessor);
 
         [Test]
         public override void CallingConstraintExistsCanAcceptConstraintNameWithSingleQuote()
         {
-            using (var table = new FirebirdTestTable(Processor, null, "id int", string.Format("wibble int CONSTRAINT {0} CHECK(wibble > 0)", "\"c'1\"")))
-                Processor.ConstraintExists(null, table.Name, "\"c'1\"").ShouldBeTrue();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable(fbProcessor, null, "id int", string.Format("wibble int CONSTRAINT {0} CHECK(wibble > 0)", "\"c'1\"")))
+                    fbProcessor.ConstraintExists(null, table.Name, "\"c'1\"").ShouldBeTrue();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsCanAcceptTableNameWithSingleQuote()
         {
-            using (var table = new FirebirdTestTable("\"Test'Table\"", Processor, null, "id int", "wibble int CONSTRAINT c1 CHECK(wibble > 0)"))
-                Processor.ConstraintExists(null, table.Name, "C1").ShouldBeTrue();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable("\"Test'Table\"", fbProcessor, null, "id int", "wibble int CONSTRAINT c1 CHECK(wibble > 0)"))
+                    fbProcessor.ConstraintExists(null, table.Name, "C1").ShouldBeTrue();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsFalseIfConstraintDoesNotExist()
         {
-            using (var table = new FirebirdTestTable(Processor, null, "id int"))
-                Processor.ConstraintExists(null, table.Name, "DoesNotExist").ShouldBeFalse();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable(fbProcessor, null, "id int"))
+                    fbProcessor.ConstraintExists(null, table.Name, "DoesNotExist").ShouldBeFalse();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsFalseIfConstraintDoesNotExistWithSchema()
         {
-            using (var table = new FirebirdTestTable(Processor, "TestSchema", "id int"))
-                Processor.ConstraintExists("TestSchema", table.Name, "DoesNotExist").ShouldBeFalse();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable(fbProcessor, "TestSchema", "id int"))
+                    fbProcessor.ConstraintExists("TestSchema", table.Name, "DoesNotExist").ShouldBeFalse();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsFalseIfTableDoesNotExist()
         {
-            Processor.ConstraintExists(null, "DoesNotExist", "DoesNotExist").ShouldBeFalse();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                processor.ConstraintExists(null, "DoesNotExist", "DoesNotExist").ShouldBeFalse();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsFalseIfTableDoesNotExistWithSchema()
         {
-            Processor.ConstraintExists("TestSchema", "DoesNotExist", "DoesNotExist").ShouldBeFalse();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                processor.ConstraintExists("TestSchema", "DoesNotExist", "DoesNotExist").ShouldBeFalse();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsTrueIfConstraintExists()
         {
-            using (var table = new FirebirdTestTable(Processor, null, "id int", "wibble int CONSTRAINT c1 CHECK(wibble > 0)"))
-                Processor.ConstraintExists(null, table.Name, "C1").ShouldBeTrue();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable(fbProcessor, null, "id int", "wibble int CONSTRAINT c1 CHECK(wibble > 0)"))
+                    fbProcessor.ConstraintExists(null, table.Name, "C1").ShouldBeTrue();
+            });
         }
 
         [Test]
         public override void CallingConstraintExistsReturnsTrueIfConstraintExistsWithSchema()
         {
-            using (var table = new FirebirdTestTable(Processor, "TestSchema", "id int", "wibble int CONSTRAINT C1 CHECK(wibble > 0)"))
-                Processor.ConstraintExists("TestSchema", table.Name, "C1").ShouldBeTrue();
+            ExecuteFor(FB_PROCESSOR, processor =>
+            {
+                var fbProcessor = processor as FirebirdProcessor;
+                using (var table = new FirebirdTestTable(fbProcessor, "TestSchema", "id int", "wibble int CONSTRAINT C1 CHECK(wibble > 0)"))
+                    fbProcessor.ConstraintExists("TestSchema", table.Name, "C1").ShouldBeTrue();
+            });
         }
     }
 }
