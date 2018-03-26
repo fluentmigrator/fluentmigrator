8d1165bd7ea7e0045be324e44c261d3a2e4cc259
Mark Junker <m.junker@fubar-dev.de>
2018-03-26 21:26:51 +0200

    Cleanup

--- src/FluentMigrator.Runner/Generators/MigrationGeneratorFactory.cs
+++ src/FluentMigrator.Runner/Generators/MigrationGeneratorFactory.cs
@@ -1,24 +1,43 @@
-﻿using System;
+﻿#region License
+//
+// Copyright (c) 2007-2009, Sean Chambers <schambers80@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+
 using FluentMigrator.Runner.Extensions;
 using FluentMigrator.Runner.Processors;
 
 namespace FluentMigrator.Runner.Generators
-{ 
-     public class MigrationGeneratorFactory
+{
+    public class MigrationGeneratorFactory
     {
-         private static readonly IDictionary<string, IMigrationGenerator> MigrationGenerators;
+        private static readonly IDictionary<string, IMigrationGenerator> MigrationGenerators;
 
         static MigrationGeneratorFactory()
         {
-            Assembly assembly = typeof (IMigrationProcessorFactory).Assembly;
+            Assembly assembly = typeof(IMigrationProcessorFactory).Assembly;
 
             List<Type> types = assembly
-                .GetExportedTypes()
-                .Where(type => type.IsConcrete() && type.Is<IMigrationGenerator>())
-                .ToList();
+                               .GetExportedTypes()
+                               .Where(type => type.IsConcrete() && type.Is<IMigrationGenerator>())
+                               .ToList();
 
             var available = new SortedDictionary<string, IMigrationGenerator>();
             foreach (Type type in types)
@@ -40,9 +59,9 @@ namespace FluentMigrator.Runner.Generators
         public virtual IMigrationGenerator GetGenerator(string name)
         {
             return MigrationGenerators
-                .Where(pair => pair.Key.Equals(name, StringComparison.OrdinalIgnoreCase))
-                .Select(pair => pair.Value)
-                .FirstOrDefault();
+                   .Where(pair => pair.Key.Equals(name, StringComparison.OrdinalIgnoreCase))
+                   .Select(pair => pair.Value)
+                   .FirstOrDefault();
         }
 
         public string ListAvailableProcessorTypes()
