0d72bdd83a4c85064bc0adef8d0f331076c6313e
Mark Junker <m.junker@fubar-dev.de>
2018-04-10 10:34:48 +0200

    Support for Microsoft.Data.Sqlite

--- src/FluentMigrator.Runner.SQLite/Processors/SQLite/SQLiteDbFactory.cs
+++ src/FluentMigrator.Runner.SQLite/Processors/SQLite/SQLiteDbFactory.cs
@@ -1,32 +1,17 @@
-using System;
-
 namespace FluentMigrator.Runner.Processors.SQLite
 {
     public class SQLiteDbFactory : ReflectionBasedDbFactory
     {
+        private static readonly TestEntry[] _testEntries =
+        {
+            new TestEntry("System.Data.SQLite", "System.Data.SQLite.SQLiteFactory"),
+            new TestEntry("Mono.Data.Sqlite, Version=4.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756", "Mono.Data.Sqlite.SqliteFactory"),
+            new TestEntry("Microsoft.Data.Sqlite", "Microsoft.Data.Sqlite.SqliteFactory"),
+        };
+
         public SQLiteDbFactory()
-            : base(DefaultAssemblyName, DefaultFactoryClassName)
+            : base(_testEntries)
         {
-        }
-
-        private static string DefaultAssemblyName
-        {
-            get
-            {
-                if (Environment.OSVersion.Platform == PlatformID.Unix)
-                    return "Mono.Data.Sqlite, Version=4.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756";
-                return "System.Data.SQLite";
-            }
-        }
-
-        private static string DefaultFactoryClassName
-        {
-            get
-            {
-                if (Environment.OSVersion.Platform == PlatformID.Unix)
-                    return "Mono.Data.Sqlite.SqliteFactory";
-                return "System.Data.SQLite.SQLiteFactory";
-            }
         }
     }
 }
