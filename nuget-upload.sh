0e73d2db831386bc94a5e48639d3800e996b565e
Mark Junker <m.junker@fubar-dev.de>
2018-04-15 09:23:18 +0200

    Use a script for MyGet uploads

---
+++ nuget-upload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "asdasd"
+for pkg in output/* ; do
+  [ -e "$pkg" ] || continue
+  nuget push "$pkg" "$MYGET_API_KEY" -Source "https://www.myget.org/F/fluent-migrator/api/v2/package"
+done
