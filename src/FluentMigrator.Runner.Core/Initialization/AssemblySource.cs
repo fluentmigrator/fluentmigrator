9ff526a80bba4d1e8878b793c6a9199cd4515800
Mark Junker <m.junker@fubar-dev.de>
2018-04-19 22:52:46 +0200

    Start of the implementation of options
    The goal is to use deprecate the IRunnerContext and use something like RunnerOptions and build the whole service provider from it.

---
+++ src/FluentMigrator.Runner.Core/Initialization/AssemblySource.cs
@@ -0,0 +1,80 @@
+#region License
+// Copyright (c) 2018, FluentMigrator Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Microsoft.Extensions.Options;
+
+namespace FluentMigrator.Runner.Initialization
+{
+    /// <summary>
+    /// Provides access to delay-loaded assemblies
+    /// </summary>
+    public class AssemblySource : IAssemblySource
+    {
+        private readonly Lazy<IReadOnlyCollection<Assembly>> _assemblies;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblySource"/> class.
+        /// </summary>
+        /// <param name="options">The options</param>
+        /// <param name="loadEngines">The assembly load engines</param>
+        [CLSCompliant(false)]
+        public AssemblySource(IOptions<AssemblySourceOptions> options, IEnumerable<IAssemblyLoadEngine> loadEngines)
+        {
+            _assemblies = new Lazy<IReadOnlyCollection<Assembly>>(() => LoadAssemblies(options.Value, loadEngines.ToList()));
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyCollection<Assembly> Assemblies => _assemblies.Value;
+
+        private static IReadOnlyCollection<Assembly> LoadAssemblies(AssemblySourceOptions options, IReadOnlyCollection<IAssemblyLoadEngine> loadEngines)
+        {
+            var result = new List<Assembly>();
+            foreach (var assemblyName in options.AssemblyNames)
+            {
+                var exceptions = new List<Exception>();
+                var added = false;
+                foreach (var loadEngine in loadEngines)
+                {
+                    if (loadEngine.TryLoad(assemblyName, exceptions, out var assembly))
+                    {
+                        result.Add(assembly);
+                        added = true;
+                        break;
+                    }
+                }
+
+                if (!added)
+                {
+                    var errorMessage = new StringBuilder($"Failed to load assembly with name {assemblyName}:").AppendLine();
+                    foreach (var exception in exceptions)
+                    {
+                        errorMessage.AppendLine(exception.Message);
+                    }
+
+                    throw new AggregateException(errorMessage.ToString(), exceptions);
+                }
+            }
+
+            return result;
+        }
+    }
+}
