cbb3089e8cba0c5dd7fda5a3ecb93518a59bc4b8
Mark Junker <m.junker@fubar-dev.de>
2018-04-20 10:27:10 +0200

    Fixed warnings and added some xmldoc comments

--- src/FluentMigrator.Abstractions/TimestampedMigrationAttribute.cs
+++ src/FluentMigrator.Abstractions/TimestampedMigrationAttribute.cs
@@ -9,16 +9,6 @@ namespace FluentMigrator
     [CLSCompliant(false)]
     public class TimestampedMigrationAttribute : MigrationAttribute
     {
-        private static readonly int[] DaysToMonth365 =
-            {
-                0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365
-            };
-
-        private static readonly int[] DaysToMonth366 =
-            {
-                0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366
-            };
-
         /// <summary>
         /// Initializes a new instance of the <see cref="TimestampedMigrationAttribute"/> class whose version is based on date and time information.
         /// </summary>
