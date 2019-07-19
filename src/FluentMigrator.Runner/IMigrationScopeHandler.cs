930fb2d2f3d912ad80e24f13e7ee8ebda1f4c67d
Dawid Trznadel <Dawid.Trznadel@fenergo.com>
2019-07-19 19:17:05 +0200

    Extracting interface

---
+++ src/FluentMigrator.Runner/IMigrationScopeHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentMigrator.Runner
+{
+    public interface IMigrationScopeHandler
+    {
+        IMigrationScope CurrentScope { get; set; }
+
+        IMigrationScope BeginScope();
+        IMigrationScope CreateOrWrapMigrationScope(bool transactional = true);
+    }
+}
