857079594e137ac6e073d68fb5d28fedd96d8a44
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-05-09 11:45:31 +0300

    add an interface for moching

--- src/FluentMigrator.Runner.Core/Initialization/QuoterOptions.cs
+++ src/FluentMigrator.Runner.Core/Initialization/QuoterOptions.cs
@@ -1,14 +1,11 @@
 namespace FluentMigrator.Runner.Initialization
 {
-    /// <summary>
-    /// Conventions of how C# values are converted to SQL
-    /// </summary>
-    public sealed class QuoterOptions
+    /// <inheritdoc cref="IQuoterOptions"/>
+    public sealed class QuoterOptions : IQuoterOptions
     {
+        /// <inheritdoc />
         /// <summary>
         /// Gets or sets the value indicating how enum values are handled.<br/>
-        /// If <see langword="true"/> enums are converted to a string, if <see langword="false"/> enums are converted to the underlying numeric type.<br/>
-        /// Default value: <see langword="true"/>.
         /// </summary>
         public bool EnumAsString { get; set; } = true;
     }
