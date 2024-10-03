0e86e199569680223e7df3db13e17bee395ce062
Shane32 <shane@acdmail.com>
2024-10-03 01:47:57 -0400

    Update

--- src/FluentMigrator.Runner.SqlServer/Generators/SqlServer/SqlServer2005Column.cs
+++ src/FluentMigrator.Runner.SqlServer/Generators/SqlServer/SqlServer2005Column.cs
@@ -30,7 +30,7 @@ namespace FluentMigrator.Runner.Generators.SqlServer
         /// <inheritdoc />
         protected override string FormatNullable(ColumnDefinition column)
         {
-            if (column.IsNullable == true && column.Type == null && !string.IsNullOrEmpty(column.CustomType))
+            if (column.IsNullable == true && column.Type == null && !string.IsNullOrEmpty(column.CustomType) && column.Expression == null)
             {
                 return "NULL";
             }
