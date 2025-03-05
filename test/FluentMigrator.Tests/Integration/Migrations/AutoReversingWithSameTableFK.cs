ed292797262ba098fbc09ab4ea2944062fc723fe
Adriano Nascimento <adriano.nascimento@sage.com>
2025-03-05 20:57:07 +0000

    Fix auto reversing with insert data (#1963)
    * FIx auto reversing with insert data
    
    * Fix broken assertion
    
    * Fix broken test
    
    * Foreign keys are not supported in SQLite
    
    ---------
    
    Co-authored-by: John Zabroski <johnzabroski@gmail.com>

---
+++ test/FluentMigrator.Tests/Integration/Migrations/AutoReversingWithSameTableFK.cs
@@ -0,0 +1,27 @@
+namespace FluentMigrator.Tests.Integration.Migrations
+{
+    [Migration(7)]
+    public class AutoReversingWithSameTableFK : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            IfDatabase(t => t != ProcessorId.SQLite)
+                .Create.Table("SameTableFK")
+                    .WithColumn("Id").AsInt32().PrimaryKey()
+                    .WithColumn("ParentId").AsInt32().Nullable()
+                        .ForeignKey("SameTableFK", "Id");
+
+            // Foreign keys are not supported in SQLite
+            IfDatabase(ProcessorId.SQLite)
+                .Create.Table("SameTableFK")
+                    .WithColumn("Id").AsInt32().PrimaryKey()
+                    .WithColumn("ParentId").AsInt32().Nullable();
+
+            Insert.IntoTable("SameTableFK")
+                .Row(new { Id = 1 })
+                .Row(new { Id = 2 })
+                .Row(new { Id = 10, ParentId = 1 })
+                .Row(new { Id = 20, ParentId = 2 });
+        }
+    }
+}
